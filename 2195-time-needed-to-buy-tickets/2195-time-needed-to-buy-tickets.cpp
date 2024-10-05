ee0ee9d2afb86e75cbf025d7710abd6352cb5cae
sourav030 <128700861+sourav030@users.noreply.github.com>
2024-10-05 18:54:03 +0530

    Time: 0 ms (100%), Space: 10.4 MB (25.37%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
@@ -4,16 +4,17 @@ public:
         queue<int>q;
         int n = tickets.size();
         for(int i=0;i<n;i++)
-        q.push(i);
+        q.push(i); // hum sare index ko queue main push krenge
 
         int Time = 0;
 
-        while(tickets[k]!=0)
+        while(tickets[k]!=0) // tickest[k] ki value 0 nhi hogi tabhi chelga our usee ticket denge
         {
-            tickets[q.front()]--;
+            tickets[q.front()]--; // useko ticket dene kr badd decrement krenge
             // Kya line mein lagega
-            if(tickets[q.front()])
+            if(tickets[q.front()]) // agar ticket[q.front ki value 0 nhi hai] use push kro qu main
             q.push(q.front());
+                // abh q.front ki delete krenge our time ko increament kr denge
             q.pop();
             Time++;
         }
