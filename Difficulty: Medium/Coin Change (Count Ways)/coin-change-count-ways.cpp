c551f8a565b1e5dcae8fcad3e6072f4be2eed40a
sourav030 <128700861+sourav030@users.noreply.github.com>
2024-10-10 12:33:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
@@ -29,8 +29,29 @@ class Solution {
     long long int count(int coins[], int N, int sum) {
 
         // code here.
-        vector<vector<long long int>>dp(N+1,vector<long long int>(sum+1,-1));
-        return find(N,sum,coins,dp);
+        // for top down approch
+        // vector<vector<long long int>>dp(N+1,vector<long long int>(sum+1,-1));
+        // return find(N,sum,coins,dp);
+        
+        // for bottom up
+        vector<vector<long long int>>dp(N+1,vector<long long int>(sum+1,-0));
+        // fill the value 1 where coloum=0
+        for(int i=0; i< N+1; i++){
+            dp[i][0]=1;
+        }
+        
+        for(int i=1; i<N+1; i++){
+            for(int j=1; j<sum+1; j++){
+                if(coins[i-1]>j){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[N][sum];
     }
 };
 
