a02674e9782816d0b88de44b305f7d58a7645c18
sourav030 <128700861+sourav030@users.noreply.github.com>
2024-11-01 08:34:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Height of Heap/height-of-heap.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int heapHeight(int N, int arr[]){
+        // code here
+        if(N==1)
+        return 1;
+        
+        int height=0;
+        while(N>1){
+            N=N/2;
+            height++;
+        }
+        return height;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() { 
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin >> N;
+        int arr[N];
+        for(int i = 0; i < N; i++)
+            cin >> arr[i];
+        
+        Solution ob;
+        cout << ob.heapHeight(N, arr) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0; 
+} 
+
+// } Driver Code Ends
