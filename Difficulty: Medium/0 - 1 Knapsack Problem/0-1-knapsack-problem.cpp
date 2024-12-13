e621c2db4055061c43cd63eca9294536f5ed8920
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2024-12-13 18:20:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -30,9 +30,34 @@ class Solution {
     int knapSack(int capacity, vector<int> &val, vector<int> &wt) {
         // code here
         int n=val.size();
-        vector<vector<int>>dp(n+1,vector<int>(capacity+1,-1));
-        int x= solve(capacity,val,wt,n,dp);
-        return x;
+        vector<vector<int>>dp(n+1,vector<int>(capacity+1));
+        
+        // ye memoization wala code hai
+        // int x= solve(capacity,val,wt,n,dp);
+        // return x;
+        
+        // ye top to down wala
+        
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<capacity+1; j++){
+                if(i==0 or j==0){
+                    dp[i][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<capacity+1; j++){
+                if(wt[i-1]<=j){
+                    dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][capacity];
+        
     }
 };
 
