b86c09ee528f9ba88fc36b8f277f110e02729fb1
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2024-12-14 09:15:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -1,51 +1,55 @@
 //{ Driver Code Starts
-//Initial template for C++
+// Initial template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-//User function Template for C++
+// User function Template for C++
 
-class Solution{
-  public:
-    int longestPalinSubseq(string A) {
-        //code here
-        int n=A.size();
-        string a=A;
-        string b=a;
-        reverse(b.begin(),b.end());
-        int m=n;
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(a[i-1]==b[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+class Solution {
+    int solve(string &s,string &s1,int n, int m ,  vector<vector<int>> &dp){
+        if (n==0 || m==0){
+            return 0;
         }
-        return dp[n][m];
-        
-        
+        if(dp[n][m]!=-1){
+            return dp[n][m];
+        }
+        if(s[n-1]==s1[m-1]){
+            return dp[n][m]= 1+solve(s,s1,n-1,m-1,dp);
+        }
+        else{
+            return dp[n][m]= max(solve(s,s1,n,m-1,dp),solve(s,s1,n-1,m,dp));
+        }
+    }
+  public:
+    int longestPalinSubseq(string &s) {
+        // code here
+        string s1=s;
+        reverse(s1.begin(),s1.end());
+        int n=s.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solve(s,s1,n,n,dp);
     }
 };
 
 //{ Driver Code Starts.
 
-int32_t main()
-{
-    int t; cin >> t;
-    while (t--)
-    {
-        string s; cin >> s;
+int32_t main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
         Solution ob;
         cout << ob.longestPalinSubseq(s) << endl;
+
+        cout << "~"
+             << "\n";
     }
 }
+
 // Contributed By: Pranay Bansal
 
 // } Driver Code Ends
