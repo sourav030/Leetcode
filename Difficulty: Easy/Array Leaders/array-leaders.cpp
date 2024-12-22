2cc0440f95fd7006a5d2224c40c53266df1b9bcc
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2024-12-22 12:44:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        // Code here
+        stack<int> st;
+        vector<int>ans;
+        for(int i=arr.size()-1; i>=0; i--){
+            if(st.empty()){
+                st.push(arr[i]);
+            }
+            else{
+                if(arr[i]>=st.top()){
+                    st.push(arr[i]);
+                }
+            }
+        }
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.leaders(a);
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no leaders are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
