448ae24d254f9206f6579d1bcb6948eb47124dee
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2024-12-22 11:54:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Score from Subarray Minimums/maximum-score-from-subarray-minimums.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to find pair with maximum sum
+    int pairWithMaxSum(vector<int> &arr) {
+        // Your code goes here
+        int result=arr[0]+arr[1];
+        for(int i=1; i<arr.size()-1; i++){
+            result=max(result,arr[i]+arr[i+1]);
+        }
+        return result;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        cout << ob.pairWithMaxSum(nums) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
