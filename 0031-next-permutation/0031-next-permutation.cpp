1caac0a95ec8730c379456bd112b0263edc80e5d
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2024-12-23 11:26:13 +0530

    Time: 0 ms (100%), Space: 15.8 MB (14.26%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-     next_permutation(nums.begin(),nums.end());
-  
+     
+
+            int index=-1;
+            int n=nums.size();
+            // Hume last smallest index find krna hai to last se traverse krenge agar mil gaya to break
+
+            for(int i=n-2; i>=0; i--){
+                if(nums[i]<nums[i+1]){
+                    index=i;
+                    break;
+                }
+            }
+            // agar indx=-1 hai to array descending order main useko reverse krke bejh denge
+            if(index==-1){
+                reverse(nums.begin(),nums.end());
+                return;
+            }
+            // ab chote element index ko swap krna hai largest ke smallest element se
+
+            for(int i=n-1; i>index; i--){
+                if(nums[i]>nums[index]){
+                    swap(nums[i],nums[index]);
+                    break;
+                }
+            }
+
+            reverse(nums.begin()+index+1,nums.end());
     }
 };
