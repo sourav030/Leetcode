15c813ee3c3bbe60f25a6ee6b160db3774627f0d
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2024-12-30 10:26:44 +0530

    Time: 0 ms (100%), Space: 26.1 MB (55.7%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
-        }
-        int start = 0;
-        int end = nums.size() - 1;
+        int start=0;
+        int end=nums.size()-1;
+        if(nums.size()==1) return nums[0];
+        if(nums[start]!=nums[start+1]) return nums[start];
+        if(nums[end]!=nums[end-1]) return nums[end];
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
 
-       
-        if (nums[start] != nums[start + 1]) return nums[start];
-        if (nums[end] != nums[end - 1]) return nums[end];
-
-       
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-           
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+            if(nums[mid]!=nums[mid+1] and nums[mid]!=nums[mid-1]){
                 return nums[mid];
             }
 
-           
-            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+           if((mid%2==1 and nums[mid]==nums[mid-1]) or (mid%2==0 and nums[mid]==nums[mid+1])){
+             start = mid + 1;
             }
+        else{
+             end = mid - 1;
         }
-
-        
+        }
         return -1;
     }
 };
