94211d6017eebb6040c7ef2e4b4edbc43ce04cb2
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-01-02 22:59:58 +0530

    Time: 0 ms (100%), Space: 13.6 MB (25.93%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,45 +1,39 @@
 class Solution {
-public:
-    bool solve(vector<vector<int>>& mat, int midd,int target){
-        
-        int start=0;
-        int end=mat[midd].size()-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(mat[midd][mid]==target){
+    bool solve(vector<int>arr, int n, int target){
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target){
                 return true;
             }
-            else if (mat[midd][mid]<target){
-                start=mid+1;
+            else if (arr[mid]<target){
+                low=mid+1;
             }
             else{
-                end=mid-1;
+                high=mid-1;
             }
         }
         return false;
     }
-    bool searchMatrix(vector<vector<int>>& mat, int target) {
-
-            if (mat.empty() || mat[0].empty()) {
-            return false; // Handle empty matrix or empty rows
-        }
-        int row=mat.size();
-        int col=mat[0].size();
-        int start=0;
-        int end=row-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-
-            if(target>=mat[mid][0] && target<=mat[mid][col-1]){
-                return solve(mat,mid,target);
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int low=0;
+        int high=row-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(matrix[mid][0]<=target and target<=matrix[mid][col-1]){
+                return solve(matrix[mid],col, target);
             }
-            else if (target >mat[mid][col-1]){
-                start=mid+1;
+            else if (target>matrix[mid][0] and target>matrix[mid][col-1]){
+                low=mid+1;
             }
             else{
-                end=mid-1;
+                high=mid-1;
             }
         }
-        return false;
+        return 0;
     }
 };
