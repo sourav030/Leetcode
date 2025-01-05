57d67b4ae86dad0dd96ac03aba3d862b5f9bf5c4
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-01-05 16:22:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.cpp
+++ Difficulty: Medium/Next Greater Element/next-greater-element.cpp
@@ -1,69 +1,61 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to find the next greater element for each element of the array.
-    vector<long long> nextLargerElement(vector<long long> arr, int n){
-        // Your code here
-        // Brute force
-        // vector<long long>ans(n,-1);
-        // for(int i=0; i<n; i++){
-        //     for(int j=i+1; j<n; j++){
-        //         if(arr[i]<arr[j]){
-        //             ans[i]=arr[j];
-                
-        //             break;
-        //         }
-        //     }
-        // }
-        
-        // return ans;
-        
+class Solution {
+  public:
+    // Function to find the next greater element for each element of the array.
+    vector<int> nextLargerElement(vector<int>& arr) {
+        // code here
         stack<int> st;
-        vector<long long>ans(n,-1);
-        
-        for(int i=0; i<n; i++){
-           
-                while( !st.empty() and arr[st.top()]<arr[i] ){
-                    
-                 ans[st.top()]=arr[i] ;
-                 st.pop();
-                 
-                }
-                st.push(i);
-            
+        int n=arr.size();
+        vector<int>ans(n,-1);
+        for(int i=arr.size()-1; i>=0; i--){
+            while(!st.empty() and st.top()<=arr[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                ans[i]=st.top();
+            }
+            st.push(arr[i]);
         }
-        
         return ans;
-        
     }
 };
 
 //{ Driver Code Starts.
 
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        
-        int n;
-        cin>>n;
-        vector<long long> arr(n);
-        for(int i=0;i<n;i++)
-            cin>>arr[i];
-        
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after reading t
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Reading the entire input line for the array
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
         Solution obj;
-        vector <long long> res = obj.nextLargerElement(arr, n);
-        for (long long i : res) cout << i << " ";
-        cout<<endl;
+        vector<int> result = obj.nextLargerElement(a);
+
+        // Print the result in the required format
+        for (int i = 0; i < result.size(); i++) {
+            if (i != 0)
+                cout << " ";
+            cout << result[i];
+        }
+        cout << endl;        // Ensure new line after each test case output
+        cout << "~" << endl; // Ensure new line after each test case output
     }
-	return 0;
+
+    return 0;
 }
+
 // } Driver Code Ends
