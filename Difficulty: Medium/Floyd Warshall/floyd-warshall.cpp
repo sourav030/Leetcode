5d5322e3b589c040c4fdca9b1d8c91568221464e
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-01-16 18:34:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -13,37 +13,37 @@ class Solution {
     void shortestDistance(vector<vector<int>>& mat) {
         // Code here
         int n=mat.size();
-        for(int k=0; k<n; k++){
+        
             for(int i=0; i<n; i++){
                 for(int j=0; j<n; j++){
                     if(mat[i][j]==-1){
-                        mat[i][j]=INT_MAX;
+                        mat[i][j]=1e9;
+                    }
+                    if(i==j){
+                        mat[i][j]=0;
                     }
                 }
             }
-        }
-        
+            
         for(int k=0; k<n; k++){
             for(int i=0; i<n; i++){
+                
                 for(int j=0; j<n; j++){
-                    if(mat[i][k]==INT_MAX or mat[k][j]==INT_MAX){
-                        continue;
-                    }
-                    mat[i][j]=min(mat[i][j], mat[i][k]+mat[k][j]);
+                   mat[i][j]=min(mat[i][j],mat[i][k]+mat[k][j]);
                 }
+                
             }
         }
         
-         for(int k=0; k<n; k++){
-            for(int i=0; i<n; i++){
+        
+        for(int i=0; i<n; i++){
                 for(int j=0; j<n; j++){
-                    if(mat[i][j]==INT_MAX){
+                    if(mat[i][j]==1e9){
                         mat[i][j]=-1;
                     }
+                   
                 }
             }
-        }
-        
     }
 };
 
