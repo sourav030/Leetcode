8e90807e6abc40d02aaee519a2f2f409269e9fb3
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-01-19 15:00:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,62 +1,60 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-class Solution
-{
-	public:
-	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-      int spanningTree(int V, vector<vector<int>> adj[])
-    {
-        // priority queue
-         // Priority queue
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-    vector<bool> IsMST(V, 0);
-    vector<int> parent(V);
-    int cost = 0;
-    
-    pq.push({0, {0, -1}});
-    
-    while (!pq.empty()) {
-        int wt = pq.top().first;
-        int node = pq.top().second.first;
-        int par = pq.top().second.second;
-        pq.pop();
+class Solution {
+  public:
+    // Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        // code here
+        priority_queue< pair<int,int>,
+        vector<pair<int,int>>,
+        greater<pair<int,int>>> pq;
         
-        if (!IsMST[node]) {
-            IsMST[node] = 1;
-            cost += wt;
-            parent[node] = par;
+        vector<bool>visited(V,0);
+        pq.push({0,0});
+        int sum=0;
+        
+        while(!pq.empty()){
+            int weight=pq.top().first;
+            int node=pq.top().second;
             
-            for (int j = 0; j < adj[node].size(); j++) {
-                if (!IsMST[adj[node][j][0]]) {
-                    pq.push({adj[node][j][1], {adj[node][j][0], node}});
+            pq.pop();
+            if(visited[node]) continue;
+            
+            visited[node]=1;
+            sum+=weight;
+            
+            for(auto it: adj[node]){
+                int root=it[0];
+                int w=it[1];
+                if(!visited[root]){
+                    pq.push({w,root});
                 }
             }
+            
         }
+        
+        return sum;
     }
-    
-    return cost;
-}
 };
 
 //{ Driver Code Starts.
 
-
-int main()
-{
+int main() {
     int t;
     cin >> t;
     while (t--) {
         int V, E;
         cin >> V >> E;
         vector<vector<int>> adj[V];
-        int i=0;
-        while (i++<E) {
+        int i = 0;
+        while (i++ < E) {
             int u, v, w;
             cin >> u >> v >> w;
-            vector<int> t1,t2;
+            vector<int> t1, t2;
             t1.push_back(v);
             t1.push_back(w);
             adj[u].push_back(t1);
@@ -64,15 +62,15 @@ int main()
             t2.push_back(w);
             adj[v].push_back(t2);
         }
-        
+
         Solution obj;
-    	cout << obj.spanningTree(V, adj) << "\n";
-    
-cout << "~" << "\n";
-}
+        cout << obj.spanningTree(V, adj) << "\n";
+
+        cout << "~"
+             << "\n";
+    }
 
     return 0;
 }
-
 
 // } Driver Code Ends
