c59c5831c0529ab8e25b4d52d11c35ec83bb5aa7
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-01-22 22:31:29 +0530

    Time: 2 ms (81.08%), Space: 16.2 MB (21.3%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,29 +1,28 @@
 class Solution {
-        void solve(vector<string>&ans,int n,int left,int right , string &s)
-        {
-                if(left+right==2*n)
-                {
-                        ans.push_back(s);
-                }
-                if(left<n)
-                {
-                        s.push_back('(');
-                        solve(ans,n,left+1,right,s);
-                        s.pop_back();
-                }
-                if(right<left){
-                        s.push_back(')');
-                        solve(ans,n,left,right+1,s);
-                        s.pop_back();
-                }
-        }
 public:
+
+    void solve(vector<string> &ans, string s, int left,int right, int n){
+        if(left+right==n*2){
+            ans.push_back(s);
+            return ;
+        }
+        if(left<n){
+            s.push_back('(');
+            solve(ans,s,left+1,right,n);
+            s.pop_back();
+        }
+        if(right<left){
+            s.push_back(')');
+            solve(ans,s,left, right+1,n);
+            s.pop_back();
+        }
+    }
     vector<string> generateParenthesis(int n) {
-        vector<string>ans;
-            int left=0;
-            int right=0;
-            string s;
-            solve(ans,n,left,right,s);
-            return ans;
+     int left=0;
+     int right=0;
+     string s="";
+     vector<string> ans;
+     solve(ans,s,left,right, n);
+     return ans;   
     }
 };
