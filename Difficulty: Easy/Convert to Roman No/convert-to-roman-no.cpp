d328b246a73a4209650dcd7be3679f0d32a84929
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-01-31 22:29:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Convert to Roman No/convert-to-roman-no.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    string convertToRoman(int n) {
+        // code here
+         vector<pair<int, string>> arr = {
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+    };
+
+    string ans = "";
+    for (int i = 0; i < arr.size(); i++) {
+        while (n >= arr[i].first) {
+            ans += arr[i].second;
+            n -= arr[i].first;
+        }
+    }
+    return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int N;
+		cin>>N;
+		Solution ob;
+    	cout << ob.convertToRoman(N) << endl;
+	
+cout << "~" << "\n";
+}
+	return 0;
+}
+// } Driver Code Ends
