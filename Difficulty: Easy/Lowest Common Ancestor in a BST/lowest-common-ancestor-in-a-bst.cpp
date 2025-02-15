ab34d87a0ecb3027a955218b6a1280d38eb50c67
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-02-15 16:02:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.cpp
+++ Difficulty: Easy/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.cpp
@@ -2,131 +2,150 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
+class Node {
+  public:
     int data;
-    Node *left;
-    Node *right;
+    Node* left;
+    Node* right;
 
+    // Constructor to initialize a new node
     Node(int val) {
         data = val;
-        left = right = NULL;
+        left = NULL;
+        right = NULL;
     }
 };
-// Function to Build Tree
 
 
 // } Driver Code Ends
-//Function to find the lowest common ancestor in a BST.
-class Solution{
-    Node* solve(Node* root, int n1, int n2){
-        if(!root) return NULL;
-        
-        if(root->data < n1 and root->data <n2){
-            solve(root->right, n1, n2);
-        }
-        
-        else if (root->data > n1 and root->data > n2){
-            solve(root->left, n1, n2);
-        }
-        else {
-            return root;
-        }
-    }
-    public:
-        Node* LCA(Node *root, int n1, int n2)
-        {
-            // code here
-            return solve(root, n1, n2);
-        }
+/*
+// Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
 
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* solve(Node* root, Node* n1, Node* n2){
+        if(!root) return NULL;
+        if(root->data>n1->data and root->data>n2->data){
+            solve(root->left,n1,n2);
+        }
+        else if(root->data<n1->data and n2->data> root->data){
+            solve(root->right,n1,n2);
+        }
+        else return root;
+    }
+    Node* LCA(Node* root, Node* n1, Node* n2) {
+        // code here
+        return solve(root,n1,n2);
+    }
 };
 
 
 //{ Driver Code Starts.
 
-Node* buildTree(string str)
-{   
+Node* buildTree(string str) {
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
-    for(string str; iss >> str; )
+    for (string str; iss >> str;)
         ip.push_back(str);
-        
+
     // Create the root of the tree
     Node* root = new Node(stoi(ip[0]));
-        
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
+    while (!queue.empty() && i < ip.size()) {
+
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the left child for the current node
             currNode->left = new Node(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
-        if(i >= ip.size())
+        if (i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the right child for the current node
             currNode->right = new Node(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
 
+Node* searchOnTree(Node* root, int num) {
+    if (root == nullptr || root->data == num)
+        return root;
+    if (num < root->data)
+        return searchOnTree(root->left, num);
+    else
+        return searchOnTree(root->right, num);
+}
 
-int main()
-{
+int main() {
 
     int t;
-	scanf("%d ",&t);
-    while(t--)
-    {
+    scanf("%d ", &t);
+    while (t--) {
         string s;
-        int l , h;
-		getline(cin,s);
-		scanf("%d ",&l);
-		scanf("%d ",&h);
+        int l, h;
+        getline(cin, s);
+        scanf("%d ", &l);
+        scanf("%d ", &h);
         Node* root = buildTree(s);
+        Node* n1 = searchOnTree(root, l);
+        Node* n2 = searchOnTree(root, h);
         Solution sol;
-        int ans = sol.LCA(root, l, h)->data;
-        cout<<ans<<endl;
-    
-cout << "~" << "\n";
-}
-    return 1;
+        int ans = sol.LCA(root, n1, n2)->data;
+        cout << ans << endl;
+        cout << "~"
+             << "\n";
+    }
+    return 0;
 }
 // } Driver Code Ends
