3dee3a153f18bd8183651245cc5d5d719ba9aab2
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-02-22 18:39:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -9,55 +9,29 @@ using namespace std;
 
 class Solution {
   public:
-  bool solve(vector<int>& arr, int target,int n, vector<vector<int>> &dp){
-      if(target==0){
-          return 1;
-      }
-      if(n<=0 or target <0)
-      {
-          return 0;
-      }
-      if(dp[n][target]!=-1){
-          return dp[n][target];
-      }
-      if(arr[n-1]>target){
-           dp[n][target]=solve(arr,target,n-1,dp);
-      }
-      else{
-          bool include=solve(arr,target-arr[n-1],n-1,dp);
-          bool exclude=solve(arr,target,n-1,dp);
-           dp[n][target]=include or exclude;
-      }
-      return dp[n][target];
-     
-  }
+    bool solve(vector<int>&arr,int target,int n, vector<vector<int>>&dp){
+        if(target==0){
+            return true;
+        }
+        if(n<=0 or target<0){
+            return false;
+        }
+        if(dp[n][target]!=-1){
+            return dp[n][target];
+        }
+        if(arr[n-1]>target){
+            dp[n][target]=solve(arr,target,n-1,dp);
+        }
+        else{
+            dp[n][target]=solve(arr,target-arr[n-1],n-1,dp)||solve(arr,target,n-1,dp);
+        }
+        return dp[n][target];
+    }
     bool isSubsetSum(vector<int>& arr, int target) {
-        // code here
-        
-        
-          int n = arr.size();
-    vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
-    // DP ko -1 se initialize karo
-    for(int i=0; i<n+1; i++){
-        for(int j=0; j<target+1; j++){
-            if(j==0){
-                dp[i][0]=1;
-            }
-        }
-    }
-    
-    for(int i=1; i<n+1; i++){
-        for(int j=1; j<target+1; j++){
-            if(arr[i-1]>j){
-                dp[i][j]=dp[i-1][j];
-            }
-            else{
-                dp[i][j]=dp[i-1][j] || dp[i-1][j-arr[i-1]];
-            }
-        }
-    }
-    return dp[n][target];
-    return solve(arr, target, n, dp);
+        // code herei
+        int n=arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+       return solve(arr,target,n,dp);
     }
 };
 
