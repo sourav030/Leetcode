b9135a6fceb1169598d69eaa5b190f5b81bc1ef2
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-03-04 17:29:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -5,36 +5,35 @@ using namespace std;
 
 // } Driver Code Ends
 
-
 class Solution {
   public:
-    // Function to find length of longest increasing subsequence.
-    int longestSubsequence(vector<int>& arr) {
-        // code here
-        unordered_set<int> s(arr.begin(), arr.end());
-vector<int> arr2(s.begin(), s.end());
-sort(arr2.begin(), arr2.end());
-
-int n = arr.size();
-int m = arr2.size();
-
-vector<int> prev(m + 1, 0), curr(m + 1, 0);
-
-for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= m; j++) {
-        if (arr[i - 1] == arr2[j - 1]) {
-            curr[j] = 1 + prev[j - 1];
-        } else {
-            curr[j] = max(prev[j], curr[j - 1]);
-        }
+    int solve(vector<int>& arr, int index, int n, int prevIndex,vector<vector<int>>&dp) {
+    if (index == n) return 0;
+    
+    if(dp[index][prevIndex+1]!=-1){
+        return dp[index][prevIndex+1];
     }
-    prev = curr;  // Move to the next row
+    // Exclude current element
+    int exclude = solve(arr, index + 1, n, prevIndex,dp);
+
+    // Include current element if it maintains increasing order
+    int include = 0;
+    if (prevIndex == -1 || arr[index] > arr[prevIndex]) {
+        include = 1 + solve(arr, index + 1, n, index,dp);
+    }
+
+     dp[index][prevIndex+1]= max(include, exclude);
+    return dp[index][prevIndex+1];
 }
 
-return prev[m];
+int lis(vector<int>& arr) {
+    int n=arr.size();
+    vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+    return solve(arr, 0,n, -1,dp);
+}
 
-    }
 };
+
 
 //{ Driver Code Starts.
 
@@ -56,7 +55,8 @@ int main() {
             arr.push_back(num);
 
         Solution obj;
-        cout << obj.longestSubsequence(arr) << endl;
+        cout << obj.lis(arr) << endl;
+        cout << "~" << endl;
     }
 
     return 0;
