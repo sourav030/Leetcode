c287ce4aaec206a4872afefc463bc32d30fb9f90
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-03-07 22:27:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -6,33 +6,41 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 // User function Template for C++
 
 class Solution {
-    int solve(string &s,string &s1,int n, int m ,  vector<vector<int>> &dp){
-        if (n==0 || m==0){
-            return 0;
-        }
-        if(dp[n][m]!=-1){
-            return dp[n][m];
-        }
-        if(s[n-1]==s1[m-1]){
-            return dp[n][m]= 1+solve(s,s1,n-1,m-1,dp);
-        }
-        else{
-            return dp[n][m]= max(solve(s,s1,n,m-1,dp),solve(s,s1,n-1,m,dp));
-        }
-    }
   public:
+    int solve(string s1,string s2,int n1, int n2, vector<vector<int>>&dp){
+        if(n1==0 or n2==0) return 0;
+        if(dp[n1][n2]!=-1) return dp[n1][n2];
+        if(s1[n1-1]==s2[n2-1]){
+          return dp[n1][n2]= 1+solve(s1,s2,n1-1,n2-1,dp);
+        }
+        return dp[n1][n2]= max(solve(s1,s2,n1-1,n2,dp),solve(s1,s2,n1,n2-1,dp));
+    }
     int longestPalinSubseq(string &s) {
         // code here
         string s1=s;
-        reverse(s1.begin(),s1.end());
+        reverse(s1.begin() ,s1.end());
         int n=s.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(s,s1,n,n,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s1[i-1]==s[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+        return solve(s1,s,n,n,dp);
     }
 };
+
 
 //{ Driver Code Starts.
 
@@ -49,7 +57,5 @@ int32_t main() {
              << "\n";
     }
 }
-
-// Contributed By: Pranay Bansal
 
 // } Driver Code Ends
