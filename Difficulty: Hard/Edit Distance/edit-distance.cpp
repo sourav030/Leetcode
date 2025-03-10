e1d6ebaf6d762bbd15e98269788979a9f6ab4150
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-03-10 18:37:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.cpp
+++ Difficulty: Hard/Edit Distance/edit-distance.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
-    
+  public:
+    // Function to compute the edit distance between two strings
     int solve(string& s1,string& s2, int n, int m, vector<vector<int>>&dp){
         if(n==0)
         {
@@ -32,20 +34,18 @@ class Solution {
         
         
     }
-  public:
-    // Function to compute the edit distance between two strings
     int editDistance(string& s1, string& s2) {
         // code here
-        int n=s1.size();
+          int n=s1.size();
         
         int m=s2.size();
         
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
         
       return  solve(s1,s2,n,m,dp);
-        
     }
 };
+
 
 //{ Driver Code Starts.
 
