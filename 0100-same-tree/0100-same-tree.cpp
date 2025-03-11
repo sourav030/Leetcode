571c8f02c8c5375bd63a65e30de88c99fdef79dc
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-03-11 21:11:30 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.64%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,17 +11,11 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* p,TreeNode* q){
-        if(p and !q) return false;
-        if(!p and q) return false;
-        if(p==NULL and q==NULL) return true;
+    bool solve(TreeNode* p, TreeNode* q){
+        if(!p and !q) return true;
+        if(!p or !q)  return false;
         if(p->val!=q->val) return false;
-        if(solve(p->left,q->left) and solve(p->right,q->right)){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return solve(p->left,q->left) and solve(p->right,q->right);
     }
     bool isSameTree(TreeNode* p, TreeNode* q) {
         return solve(p,q);
