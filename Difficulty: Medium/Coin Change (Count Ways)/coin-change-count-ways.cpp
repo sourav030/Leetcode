89c843ae13dc88d24e2e3789e6dcaa5811f535de
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-03-14 17:30:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
@@ -4,48 +4,31 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-  int solve(vector<int>& coins, int sum,int n,vector<vector<int>>dp){
-      if(sum==0){
-          return 1;
-      }
-      if(n==0 ){
-          return 0;
-      }
-      if(dp[n][sum]!=-1){
-          return dp[n][sum];
-      }
-      if(coins[n-1] > sum){
-          return dp[n][sum]= solve(coins,sum,n-1,dp);
-      }
-      else{
-          dp[n][sum]= solve(coins,sum-coins[n-1],n,dp)+solve(coins,sum, n-1,dp);
-      }
-       return dp[n][sum];
-  }
+    int solve(vector<int>& coins,int sum,int index,vector<vector<int>>&dp){
+        if(sum==0) return 1;
+        if(index>=coins.size() or sum<0) return 0;
+        if(dp[index][sum]!=-1) return dp[index][sum];
+        int include=0;
+        int exclude=0;
+        if(coins[index]>sum){
+            exclude=solve(coins,sum,index+1,dp);
+        }
+        else{
+            include=solve(coins,sum-coins[index],index,dp);
+            exclude=solve(coins,sum,index+1,dp);
+        }
+        return dp[index][sum]= include+exclude;
+    }
     int count(vector<int>& coins, int sum) {
         // code here.
-    
-        int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,-0));
-        for(int i=0; i<=n; i++){
-            dp[i][0]=1;
-        }
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=sum; j++){
-                if(coins[i-1]>j){
-                    dp[i][j]=dp[i-1][j];
-                }
-                else{
-                    dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][sum];
-        return solve(coins,sum,n,dp);
+        vector<vector<int>>dp(coins.size()+1,vector<int>(sum+1,-1));
+        return solve(coins,sum,0,dp);
     }
 };
+
 
 //{ Driver Code Starts.
 
