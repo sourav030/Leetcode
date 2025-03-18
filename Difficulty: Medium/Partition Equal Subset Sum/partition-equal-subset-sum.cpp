13ebf3a1feaadb7bf23e1fec3d7701c79e27039f
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-03-18 18:30:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
@@ -5,50 +5,44 @@ using namespace std;
 
 
 // } Driver Code Ends
-// User function Template for C++
 
 class Solution {
   public:
-  bool solve(vector<int>arr, int sum, int n,  vector<vector<int>> &dp){
-      if(sum==0){
-          return 1;
-      }
-      if(n<=0 or sum < 0){
-          return 0;
-      }
-      
-      if(dp[n][sum]!=-1){
-          return dp[n][sum];
-      }
-      
-      if(arr[n-1]>sum){
-         dp[n][sum]=solve(arr,sum,n-1,dp);
-          return dp[n][sum];
-          
-      }
-      
-      else{
-          bool include=solve(arr,sum-arr[n-1],n-1,dp);
-          bool exclude=solve(arr,sum,n-1,dp);
-          return dp[n][sum]= include || exclude;
-      }
-  }
-    bool equalPartition(vector<int>& arr) {
-        // code here
+  
+    bool solve(vector<int>& arr,int sum, int n,vector<vector<int>>&dp){
+        if(sum==0)return 1;
+        if(n<=0)return 0;
+        bool include=0;
+        bool exclude=0;
+        if(dp[n][sum]!=-1) return dp[n][sum];
+        if(sum<arr[n-1]){
+            exclude=solve(arr,sum,n-1,dp);
+        }
+        else{
+            include=solve(arr,sum-arr[n-1],n-1,dp);
+            exclude=solve(arr,sum,n-1,dp);
+        }
+        return dp[n][sum]= include||exclude;
+    }
+    
+    int SumofArray(vector<int>& arr){
         int sum=0;
         for(int i=0; i<arr.size(); i++){
             sum+=arr[i];
         }
-        if(sum%2!=0){
-            return 0;
-        }
-        int x=sum/2;
-        
-       int n=arr.size();
-        vector<vector<int>>dp(n+1,vector<int>(x+1,-1));
-       return solve(arr,sum/2,n,dp);
+        return sum;
+    }
+    bool equalPartition(vector<int>& arr) {
+        // code here
+        int sum=SumofArray(arr);
+        if(sum%2!=0) return false;
+        int n=arr.size();
+        int m=sum/2;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return solve(arr,sum/2,n,dp);
     }
 };
+
 
 //{ Driver Code Starts.
 
