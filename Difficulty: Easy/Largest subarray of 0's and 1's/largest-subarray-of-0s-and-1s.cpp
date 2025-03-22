f400807cc58b224675705aeabcb490ae06fa9a52
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-03-22 18:38:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Largest subarray of 0's and 1's/largest-subarray-of-0s-and-1s.cpp
+++ Difficulty: Easy/Largest subarray of 0's and 1's/largest-subarray-of-0s-and-1s.cpp
@@ -4,36 +4,34 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    
+    int maxLen(vector<int> &arr) {
         // Your code here
-        int maxLen(vector<int> &arr) {
-    for(int i = 0; i < arr.size(); i++) {
-        if(arr[i] == 0) arr[i] = -1;
-    }
-    
-    int ans = 0;
-    unordered_map<int, int> mp;
-    int sum = 0;
-    
-    for(int i = 0; i < arr.size(); i++) {
-        sum += arr[i];
-        if(sum == 0) {
-            ans = max(ans, i + 1);
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]==0) arr[i]=-1;
         }
-        if(mp.find(sum) != mp.end()) {
-            ans = max(ans, i - mp[sum]);
-        } else {
-            mp[sum] = i;
+        
+        unordered_map<int,int>mp;
+        int sum=0;
+        int ans=0;
+        
+        for(int i=0; i<arr.size();i++){
+            sum+=arr[i];
+            if(sum==0){
+                ans=max(ans,i-0+1);
+            }
+            if(mp.find(sum)!=mp.end()){
+                ans=max(ans,i-mp[sum]);
+            }
+            else
+            mp[sum]=i;
         }
+        return ans;
     }
-    
-    return ans;
-}
-
-    
 };
+
 
 
 //{ Driver Code Starts.
