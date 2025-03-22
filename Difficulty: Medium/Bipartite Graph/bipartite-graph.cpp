ed01978c8d8ce430ffdb83fc70683dd201328d81
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-03-23 00:37:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
@@ -1,61 +1,72 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-class Solution {
-public:
-	bool isBipartite(int V, vector<int>adj[]){
-	    // Code here
-	    vector<int> color(V,-1);
-	    queue<int>q;
-	    for(int i=0; i<V; i++){
-	        if(color[i]==-1){
-	            color[i]=0;
-	            q.push(i);
-	            while(!q.empty()){
-	                int Node=q.front();
-	                q.pop();
-	                for(int i=0; i<adj[Node].size(); i++){
-	                    if(color[adj[Node][i]]==-1){
-	                        color[adj[Node][i]]=(color[Node]+1)%2;
-	                        q.push(adj[Node][i]);
-	                    }
-	                    else {
-	                        if(color[Node]==color[adj[Node][i]]){
-	                            return 0;
-	                        }
-	                    }
-	                }
-	            }
-	        }
-	    }
-	    return 1;
-	}
 
+// } Driver Code Ends
+
+class Solution {
+  public:
+   bool isBipartite(vector<vector<int>>& adj) {
+    int V = adj.size();
+    vector<int> colors(V, -1);  // Use int instead of bool (-1 for uncolored)
+    vector<bool> visited(V, false);
+    
+    queue<pair<int, int>> q;
+
+    for (int i = 0; i < V; i++) { // Check for disconnected components
+        if (!visited[i]) {
+            q.push({i, 0});
+            visited[i] = true;
+            colors[i] = 0;
+
+            while (!q.empty()) {
+                int node = q.front().first;
+                int color = q.front().second;
+                q.pop();
+
+                for (auto neigh : adj[node]) {
+                    if (colors[neigh] == color) { 
+                        return false; // Conflict detected, graph is NOT bipartite
+                    }
+                    if (!visited[neigh]) {
+                        visited[neigh] = true;
+                        colors[neigh] = !color; // Assign opposite color
+                        q.push({neigh, !color});
+                    }
+                }
+            }
+        }
+    }
+    return true; // If no conflicts found, graph is bipartite
+}
 };
 
+
 //{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int V, E;
-		cin >> V >> E;
-		vector<int>adj[V];
-		for(int i = 0; i < E; i++){
-			int u, v;
-			cin >> u >> v;
-			adj[u].push_back(v);
-			adj[v].push_back(u);
-		}
-		Solution obj;
-		bool ans = obj.isBipartite(V, adj);    
-		if(ans)cout << "1\n";
-		else cout << "0\n";  
-	
-cout << "~" << "\n";
-}
-	return 0;
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj(V);
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isBipartite(adj);
+        if (ans)
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
 }
 // } Driver Code Ends
