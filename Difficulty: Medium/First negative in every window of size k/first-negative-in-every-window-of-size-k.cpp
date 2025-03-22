e15e60b48b7c2b363d59af5e2c73759a74384fe6
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-03-22 16:33:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
@@ -2,65 +2,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<long long> printFirstNegativeInteger(long long int arr[],
-                                             long long int n, long long int k);
-
-// Driver program to test above functions
-int main() {
-    long long int t, i;
-    cin >> t;
-    while (t--) {
-        long long int n;
-        cin >> n;
-        long long int arr[n];
-        for (i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        long long int k;
-        cin >> k;
-
-        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
-        for (auto it : ans) cout << it << " ";
-        cout << endl;
-    }
-    return 0;
-}
 
 // } Driver Code Ends
 
 
 
-vector<long long> printFirstNegativeInteger(long long int A[],
-                                             long long int N, long long int k) {
-                                                 
-                            vector<long long>ans;
-                            queue<long long> q;
-                            for(int i=0; i<k-1; i++){
-                                if(A[i]<0){
-                                     q.push(i);
-                                }
-                               
-                                
-                            }
-                            for(int i=k-1; i<N; i++){
-                                if(A[i]<0){
-                                     q.push(i);
-                                }
-                                if(q.empty()){
-                                    ans.push_back(0);
-                                }
-                                else{
-                                    if(q.front()<=i-k){
-                                        q.pop();
-                                    }
-                                    if(q.empty()){
-                                        ans.push_back(0);
-                                    }
-                                    else {
-                                        ans.push_back(A[q.front()]);
-                                    }
-                                }
-                            }
-                            return ans;
-                                                 
- }
+class Solution {
+  public:
+    vector<int> FirstNegativeInteger(vector<int>& arr, int k) {
+        // write code here
+        vector<int>ans;
+        vector<int>subarray;
+        int zero=0;
+        for(int i=0; i<k; i++){
+            if(arr[i]<0 and zero==0){
+                zero=arr[i];
+            }
+            subarray.push_back(arr[i]);
+        }
+        ans.push_back(zero);
+        
+        for(int i=k; i<arr.size(); i++){
+            subarray.push_back(arr[i]);
+            subarray.erase(subarray.begin());
+            
+            zero=0;
+            for(int i=0; i<subarray.size(); i++){
+                if(subarray[i]<0){
+                    zero=subarray[i];
+                    break;
+                }
+            }
+            ans.push_back(zero);
+            
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.FirstNegativeInteger(arr, k);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
