8062cff7f1634cb309537093505f24a4a2e64291
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-03-24 23:50:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Vertical Tree Traversal/vertical-tree-traversal.cpp
+++ Difficulty: Medium/Vertical Tree Traversal/vertical-tree-traversal.cpp
@@ -3,162 +3,132 @@
 using namespace std;
 
 // Tree Node
-struct Node
-{
+class Node {
+  public:
     int data;
     Node* left;
     Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
 };
-// Utility function to create a new Tree Node
-Node* newNode(int val)
-{
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-    
-    return temp;
-}
 
 // Function to Build Tree
-Node* buildTree(string str)
-{   
+Node* buildTree(string str) {
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
-    for(string str; iss >> str; )
+    for (string str; iss >> str;)
         ip.push_back(str);
-        
+
     // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-          
+    Node* root = new Node(stoi(ip[0]));
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
+    while (!queue.empty() && i < ip.size()) {
+
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-                
+            currNode->left = new Node(stoi(currVal));
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
-        if(i >= ip.size())
+        if (i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-                
+            currNode->right = new Node(stoi(currVal));
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
 
 // Function for Inorder Traversal
-void printInorder(Node* root)
-{
-    if(!root)
+void printInorder(Node* root) {
+    if (!root)
         return;
-        
+
     printInorder(root->left);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     printInorder(root->right);
 }
 
 
 // } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to find the vertical order traversal of Binary Tree.
-    void find(Node* root,int index,int &l, int &r){
-        if(root==NULL) return ;
-        l=min(l,index);
-        r=max(r,index);
-        
-        find(root->left, index-1, l, r);
-        find(root->right, index+1,l, r);
+
+/*
+// Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
     }
-    vector<int> verticalOrder(Node *root)
-    {
-        //Your code here
-        int l=0; 
-        int r=0;
-        find(root,0,l,r);
-        vector<vector<int>>Positive(r+1);
-        vector<vector<int>>Negative(abs(l)+1);
+};
+*/
+
+class Solution {
+  public:
+    map<int,vector<int>>mp;
+    void solve(Node* root, int index){
+        if(!root) return ;
         
-        queue<Node*> q;
-        q.push(root);
-        queue<int> index;
-        index.push(0);
-        
-        while(!q.empty()){
-            Node* temp=q.front();
-            q.pop();
-            int pos=index.front();
-            index.pop();
-            
-            if(pos>=0){
-                Positive[pos].push_back(temp->data);
-            }
-            else{
-                Negative[abs(pos)].push_back(temp->data);
-            }
-            
-            if(temp->left)
-              {q.push(temp->left);
-              index.push(pos-1);}
-              
-              if(temp->right){
-                  q.push(temp->right);
-                  index.push(pos+1);
-              }
-        }
-        
-        vector<int>ans;
-        for(int i=Negative.size()-1; i>=0; i--){
-            for(int j=0; j<Negative[i].size(); j++){
-                ans.push_back(Negative[i][j]);
-            }
-        }
-        
-          for(int i=0; i<Positive.size(); i++){
-            for(int j=0; j<Positive[i].size(); j++){
-                ans.push_back(Positive[i][j]);
-            }
+        mp[index].push_back(root->data);
+        solve(root->left,index-1);
+        solve(root->right,index+1);
+    }
+    vector<vector<int>> verticalOrder(Node *root) {
+        // Your code her
+        solve(root,0);
+        vector<vector<int>>ans;
+        for(auto it:mp){
+            ans.push_back(it.second);
         }
         return ans;
+        
     }
 };
 
@@ -167,27 +137,30 @@ class Solution
 //{ Driver Code Starts.
 int main() {
     int t;
-    string  tc;
-    getline(cin,tc);
-    t=stoi(tc);
-    while(t--)
-    {
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
         string s;
-        getline(cin,s);
+        getline(cin, s);
         // string c;
         // getline(cin,c);
         Solution obj;
-    	Node* root = buildTree(s);
-    	
-    	vector <int> res = obj.verticalOrder(root);
-    	for (int i : res) cout << i << " ";
-        cout << endl;
-    
-cout << "~" << "\n";
-}
-	return 0;
-}
+        Node* root = buildTree(s);
 
-
+        vector<vector<int>> res = obj.verticalOrder(root);
+        cout << "[ ";
+        for (auto& vec : res) {
+            cout << "[ ";
+            for (int val : vec) {
+                cout << val << " ";
+            }
+            cout << "] ";
+        }
+        cout << "]" << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
 
 // } Driver Code Ends
