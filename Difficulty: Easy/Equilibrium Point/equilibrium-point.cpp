48f609f5f6d13bbb70bd64991108489c923e9238
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-04-06 22:50:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Equilibrium Point/equilibrium-point.cpp
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.cpp
@@ -5,26 +5,31 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     // Function to find equilibrium point in the array.
     int findEquilibrium(vector<int> &arr) {
         // code here
+        vector<int>prefix;
         int sum=0;
         for(int i=0; i<arr.size(); i++){
             sum+=arr[i];
+            prefix.push_back(sum);
         }
-        
-        int prefix=0;
-        for(int i=0; i<arr.size(); i++){
-           sum-=arr[i];
-           if(sum==prefix) return i;
-           prefix+=arr[i];
+        int n=prefix.size();
+        int total_sum=prefix[n-1];
+        for(int i=1; i<n; i++){
+            int left=prefix[i-1];
+            int right=total_sum-prefix[i];
+            if(left==right){
+                return i;
+            }
         }
-        
         return -1;
     }
 };
+
 
 //{ Driver Code Starts.
 
