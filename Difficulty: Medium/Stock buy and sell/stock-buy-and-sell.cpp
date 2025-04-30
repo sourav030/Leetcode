e784e992e7647ccb6c21da504e4f046644ae9c7d
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-04-30 16:30:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock buy and sell/stock-buy-and-sell.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to find the days of buying and selling stock for max profit.
+    int stockBuySell(vector<int> &arr) {
+        // code here
+        int ans=0;
+        int buy=arr[0];
+        for(int i=1; i<arr.size(); i++){
+            if(arr[i]>arr[i-1]){
+                ans+=arr[i]-arr[i-1];
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.stockBuySell(arr);
+        cout << res << endl;
+        cout << "~" << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
