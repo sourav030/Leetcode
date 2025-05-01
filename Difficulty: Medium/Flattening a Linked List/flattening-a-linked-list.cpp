9b5d2969ba41ad5a29fac5ca39686ee2829a917e
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-05-01 16:30:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -1,29 +1,34 @@
 //{ Driver Code Starts
+//Initial Template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
 struct Node {
     int data;
-    struct Node *next;
-    struct Node *bottom;
+    struct Node * next;
+    struct Node * bottom;
 
     Node(int x) {
         data = x;
         next = NULL;
         bottom = NULL;
     }
+
 };
 
-void printList(Node *Node) {
-    while (Node != NULL) {
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
         printf("%d ", Node->data);
         Node = Node->bottom;
     }
-    printf("\n");
 }
 
 
 // } Driver Code Ends
+
 /* Node structure  used in the program
 
 struct Node{
@@ -42,102 +47,120 @@ struct Node{
 
 class Solution {
   public:
-    // Function which returns the  root of the flattened linked list.
-    
-    Node* merge(Node* head1, Node* head2){
-        Node* head=new Node(-1);
-        Node* tail=head;
-        while(head1 and head2){
-            if(head1->data<=head2->data){
-                tail->bottom=head1;
-                head1=head1->bottom;
-                tail=tail->bottom;
-                tail->bottom=NULL;
+    Node* linklist(Node* root, Node* root1){
+        Node* ans=new Node(-1);
+        Node* newNode=ans;
+        
+        while(root and root1){
+            if(root->data<root1->data){
+                Node* new_node=new Node(root->data);
+            newNode->bottom=new_node;
+            root=root->bottom;
+            newNode=newNode->bottom;
             }
             else{
-                tail->bottom=head2;
-                head2=head2->bottom;
-                tail=tail->bottom;
-                tail->bottom=NULL;
+            Node* new_node=new Node(root1->data);
+            newNode->bottom=new_node;
+            root1=root1->bottom;
+            newNode=newNode->bottom;
             }
+          
+        }
+        while(root){
+             Node* new_node=new Node(root->data);
+            newNode->bottom=new_node;
+            root=root->bottom;
+            newNode=newNode->bottom;
         }
         
-        if(head1){
-            tail->bottom=head1;
+        while(root1){
+            Node* new_node=new Node(root1->data);
+            newNode->bottom=new_node;
+            root1=root1->bottom;
+            newNode=newNode->bottom;
         }
-        if(head2){
-            tail->bottom=head2;
-        }
-        return head->bottom;
+        return ans->bottom;
     }
-    
     Node *flatten(Node *root) {
         // Your code here
-        Node *head1, *head2, *head3;
-        while(root->next){
-            head1=root;
-            head2=root->next;
-            head3=root->next->next;
-            head1->next=NULL;
-            head2->next=NULL;
-            
-           root= merge(head1,head2);
-           root->next=head3;
-            
+        Node* head=root;
+        root=root->next;
+        while(root){
+            head=linklist(root,head);
+            root=root->next;
         }
-        return root;
+        return head;
+        
     }
 };
 
 
+
 //{ Driver Code Starts.
 
-int main() {
+int main(void) {
+
     int t;
     cin >> t;
     while (t--) {
-        int n;
-        vector<int> work;
-        string input;
-        getline(cin, input); // Read the entire line for the array elements
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            work.push_back(number);
-        }
-        n = work.size();
-
-        Node *head = NULL;
-        Node *pre = NULL;
-
+        int n, m, flag = 1, flag1 = 1;
+        struct Node * temp = NULL;
+        struct Node * head = NULL;
+        struct Node * pre = NULL;
+        struct Node * tempB = NULL;
+        struct Node * preB = NULL;
+        cin >> n;
+        int work[n];
+        for (int i = 0; i < n; i++)
+            cin >> work[i];
         for (int i = 0; i < n; i++) {
-            int m = work[i] - 1;
+            m = work[i];
+            --m;
             int data;
-            cin >> data;
-            Node *temp = new Node(data);
-            if (head == NULL) {
+            scanf("%d", &data);
+            temp = new Node(data);
+            temp->next = NULL;
+            temp->bottom = NULL;
+
+            if (flag) {
                 head = temp;
                 pre = temp;
-            } else {
+                flag = 0;
+                flag1 = 1;
+            }
+            else {
                 pre->next = temp;
                 pre = temp;
+                flag1 = 1;
             }
-
-            Node *preB = temp;
             for (int j = 0; j < m; j++) {
+
                 int temp_data;
-                cin >> temp_data;
-                Node *tempB = new Node(temp_data);
-                preB->bottom = tempB;
-                preB = tempB;
+                scanf("%d", &temp_data);
+                tempB = new Node(temp_data);
+
+                if (flag1) {
+                    temp->bottom = tempB;
+                    preB = tempB;
+                    flag1 = 0;
+                }
+                else {
+                    preB->bottom = tempB;
+                    preB = tempB;
+                }
             }
         }
+        Node *fun = head;
+        Node *fun2 = head;
 
         Solution ob;
-        Node *root = ob.flatten(head);
+        Node* root = ob.flatten(head);
         printList(root);
-    }
+        cout << endl;
+
+    
+cout << "~" << "\n";
+}
     return 0;
 }
 
