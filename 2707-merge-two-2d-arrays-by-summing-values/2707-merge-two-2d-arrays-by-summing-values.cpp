5629a8c0019b6b6071173c1a72663db28092c5f6
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-05-04 21:57:29 +0530

    Time: 5 ms (9.89%), Space: 14.6 MB (96.47%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        for(int i=0; i<nums2.size(); i++){
-            nums1.push_back(nums2[i]);
-        }
-        int n=nums1.size();
-        vector<vector<int>>ans;
-        map<int,vector<int>>mp;
-        for(int i=0; i<nums1.size(); i++){
-            mp[nums1[i][0]].push_back(nums1[i][1]);
+   vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        vector<vector<int>> answer;
+        int i = 0, j = 0;
+        while(i < nums1.size() && j < nums2.size()) {
+            int id1 = nums1[i][0], val1 = nums1[i][1];
+            int id2 = nums2[j][0], val2 = nums2[j][1];
+
+            if(id1 < id2) {
+                answer.push_back({id1,val1});
+                i++;
+            } else if(id1 > id2) {
+                answer.push_back({id2,val2});
+                j++;
+            } else {
+                answer.push_back({id1,val1+val2});
+                i++,j++;
+            }
         }
 
-        for(auto ele: mp){
-            int id=ele.first;
-            int val=0;
-            for(int i: ele.second){
-                val+=i;
-            }
-            ans.push_back({id,val});
+        while(i<nums1.size()) {
+            answer.push_back(nums1[i]);
+            i++;
         }
-        
-        return ans;
+
+        while(j<nums2.size()) {
+            answer.push_back(nums2[j]);
+            j++;
+        }
+
+        return answer;
     }
+
 };
