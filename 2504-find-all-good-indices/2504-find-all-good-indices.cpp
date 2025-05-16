8409cb2f2969486af048308aa42d92841d2c7565
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-05-16 15:47:18 +0530

    Time: 12 ms (28.43%), Space: 93.5 MB (69.93%) - LeetHub

--- 2504-find-all-good-indices/2504-find-all-good-indices.cpp
+++ 2504-find-all-good-indices/2504-find-all-good-indices.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     vector<int> goodIndices(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> nonInc(n, 1), nonDec(n, 1);
-
-        // Build non-increasing prefix
-        for (int i = 1; i < n; i++) {
-            if (nums[i] <= nums[i - 1]) {
-                nonInc[i] = nonInc[i - 1] + 1;
-            } else {
-                nonInc[i] = 1;
+        int n=nums.size();
+        vector<int>decre(n,1);
+        vector<int>incre(n,1);
+        for(int i=1; i<n; i++){
+            if(nums[i]<=nums[i-1]){
+                decre[i]=decre[i-1]+1;
+            }
+            else{
+                decre[i]=1;
             }
         }
 
-        // Build non-decreasing suffix
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] <= nums[i + 1]) {
-                nonDec[i] = nonDec[i + 1] + 1;
-            } else {
-                nonDec[i] = 1;
+        for(int j=n-2; j>=0; j--){
+            if(nums[j]<=nums[j+1]){
+                incre[j]=incre[j+1]+1;
+            }
+            else{
+                incre[j]=1;
             }
         }
 
         vector<int> ans;
-        for (int i = k; i < n - k; i++) {
-            if (nonInc[i - 1] >= k && nonDec[i + 1] >= k) {
+
+        for(int i = k; i < n - k; i++) {
+            if (incre[i + 1] >= k && decre[i - 1] >= k) {
                 ans.push_back(i);
             }
         }
+
         return ans;
+
     }
 };
