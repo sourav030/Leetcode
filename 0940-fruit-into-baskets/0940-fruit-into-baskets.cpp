dc695ad22441bc9c89e7bc04e5026c1779b31df2
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-05-24 16:34:02 +0530

    Time: 52 ms (18.99%), Space: 75.1 MB (32.66%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -8,7 +8,7 @@ public:
         while(end<fruits.size()){
             mp[fruits[end]]++;
             if(mp.size()>2){
-                while(mp.size()>2){
+                if(mp.size()>2){
                     mp[fruits[start]]--;
                     if(mp[fruits[start]]==0){
                         mp.erase(fruits[start]);
