e1e8f76008b06cc042d2f54463acb1708083dabd
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-05-30 19:42:19 +0530

    Time: 12 ms (31.22%), Space: 44.7 MB (74.25%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,21 +3,14 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        int i=0;// greedy factor
-        int j=0;// size of cookie
-        int n=g.size();
-        int m=s.size();
-        int ans=0;
-        while(j<m){
-            if(i<n){
-                 if(g[i] <= s[j]){
-                i+=1;
-                ans+=1;
+        int child=0;
+        int cookie=0;
+        while(child<g.size() and cookie<s.size()){
+            if(g[child]<=s[cookie]){
+                child++;
             }
-            }
-           
-            j+=1;
+            cookie++;
         }
-        return ans;
+        return child;
     }
 };
