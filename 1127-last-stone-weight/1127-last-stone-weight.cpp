57163ea8923914cbdf57a4c16e2223e85613590c
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-06-22 22:29:13 +0530

    Time: 0 ms (100%), Space: 10 MB (45.04%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(int i=0; i<stones.size(); i++){
+            pq.push(stones[i]);
+        }
+        while(pq.size()>1){
+            int a=pq.top();
+            pq.pop();
+            int b=pq.top();
+            pq.pop();
+            int ele=abs(a-b);
+          
+                pq.push(ele);
+         
+        }
+        return pq.top();
+    }
+};
