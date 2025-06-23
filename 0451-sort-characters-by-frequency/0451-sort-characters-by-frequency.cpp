1e5906a93811e2ccd83ee853bc5342d963961838
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-06-23 09:44:19 +0530

    Time: 5 ms (31.08%), Space: 11.6 MB (20.98%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,8 +1,5 @@
 class Solution {
 public:
-    static bool comp(pair<int,char>a, pair<char,int>b){
-        return a.first>b.first;
-    }
     string frequencySort(string s) {
         unordered_map<char,int>mp;
         for(auto ele:s){
@@ -10,14 +7,14 @@ public:
         }
         vector<pair<int,char>>arr;
         for(auto ele:mp){
-            arr.push_back({ele.second,ele.first});
+           arr.push_back({ele.second, ele.first});
         }
-        sort(arr.begin(),arr.end());
-        reverse(arr.begin(),arr.end());
-        string result="";
-        for(auto [fre,ch]:arr){
-            result+=string(fre,ch);
+     sort(arr.begin(), arr.end(), greater<>());
+        
+        string ans="";
+       for (auto& [freq, ch] : arr) {
+            ans += string(freq, ch); // Append character 'ch' freq times
         }
-        return result;
+        return ans;
     }
 };
