2f197bb978f415a00c9d7a60101b0c07a1f80b59
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-06-25 12:09:16 +0530

    Time: 3 ms (19.59%), Space: 16.4 MB (69.16%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,22 +11,18 @@
  */
 class Solution {
 public:
-    TreeNode* bst(vector<int>preorder, int &index, int low, int up)
-    {
-        if(index >= preorder.size() or low > preorder[index] or up < preorder[index])
+    TreeNode* buildTree(vector<int>&preorder, int &idx, int mini, int maxi){
+        if(idx>preorder.size()-1 or preorder[idx]<mini or preorder[idx]>maxi)
         {
-            return NULL;
+            return nullptr;
         }
-        TreeNode* root= new TreeNode(preorder[index++]);
-        root->left=bst(preorder ,index,low, root->val);
-        root->right=bst(preorder,index,  root->val , up);
+        TreeNode* root= new TreeNode(preorder[idx++]);
+        root->left=buildTree(preorder, idx, mini,root->val);
+        root->right=buildTree(preorder, idx , root->val, maxi);
         return root;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        int index=0;
-        return  bst(preorder, index, INT_MIN, INT_MAX);
-        // approch
-        // data is insert at left if its satify this condition data is greater than int_min and samller the root val
-  // insert at right side if value is greater than root val and smaller than infinity      
+        int idx=0;
+        return buildTree(preorder,idx, INT_MIN, INT_MAX);
     }
 };
