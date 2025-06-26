23637d8b6696c2f126afe87cf967ff385cf424ae
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-06-26 16:02:27 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,46 +1,46 @@
 class Solution {
 public:
+    int findFirst(vector<int>&nums, int target){
+        int ans=-1;
+        int low=0;
+        int end=nums.size()-1;
+        while(low<=end){
+            int mid=low+(end-low)/2;
+            if(nums[mid]>target){
+                end=mid-1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                ans=mid;
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+    int findLast(vector<int>&nums, int target){
+        int ans=-1;
+        int low=0;
+        int end=nums.size()-1;
+        while(low<=end){
+            int mid=low+(end-low)/2;
+            if(nums[mid]>target){
+                end=mid-1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                ans=mid;
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-            
-            // first occurance hamesa left side hoga to element ke mileane ke bad fir se left side check krenge
-// last occurance hamesa right side hoga to element milane ke bad bhi hum right side dkhenge
-        int first=-1;
-            int second=-1;
-            int f=0;
-            int end=nums.size()-1;
-            while(f<=end){
-                    int mid=f+(end-f)/2;
-                    if(nums[mid]> target)
-                    {
-                            end=mid-1;
-                    }
-                    else if(nums[mid]< target)
-                    {
-                            f=mid+1;
-                    }
-                    else{
-                            first=mid;
-                            end=mid-1;
-                    }
-            }
-            
-            f=0;
-            end=nums.size()-1;
-                 while(f<=end){
-                    int mid=f+(end-f)/2;
-                    if(nums[mid]> target)
-                    {
-                            end=mid-1;
-                    }
-                    else if(nums[mid]< target)
-                    {
-                            f=mid+1;
-                    }
-                    else{
-                            second=mid;
-                            f=mid+1;
-                    }
-            }
-            return {first,second};
+         int a=findFirst(nums,target);
+         int b = findLast(nums, target);
+         return {a,b};
     }
 };
