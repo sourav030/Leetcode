9b70da8fc9753b39bc0f4f16dd05ff0927cf0fbe
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-06-27 21:52:29 +0530

    Time: 3 ms (10.65%), Space: 15.1 MB (46.38%) - LeetHub

--- 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.cpp
@@ -1,12 +1,52 @@
 class Solution {
 public:
-    vector<int> targetIndices(vector<int>& nums, int target) {
-        vector<int>ans;
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==target){
-                ans.push_back(i);
+    int lowerBound(vector<int>&nums, int target){
+        int low=0;
+        int ans=-1;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                high=mid-1;
             }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    int UpperBound(vector<int>&nums, int target){
+        int low=0;
+        int high=nums.size()-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end()); 
+        int i=lowerBound(nums,target);
+        int j=UpperBound(nums,target);
+        
+        vector<int>ans;
+        if(i==-1 or j==-1) return ans;
+        for(int k=i; k<=j; k++){
+            ans.push_back(k);
         }
         return ans;
     }
