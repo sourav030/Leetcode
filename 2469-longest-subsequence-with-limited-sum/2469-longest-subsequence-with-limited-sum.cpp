d15e45a81505a458e329f60429de44b4d2192056
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-06-27 09:50:49 +0530

    Time: 4 ms (49.94%), Space: 18.1 MB (33.6%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
-        vector<int>ans;
-        sort(nums.begin(),nums.end());
-        for(int i=0; i<queries.size(); i++){
-            int count=0;
-            int sum=0;
-            for(int j=0; j<nums.size(); j++ ){
-                sum+=nums[j];
-                if(sum<=queries[i]){
-                    count++;
-                }
-                else{
-                    break;
+        sort(nums.begin(), nums.end());
+
+        // Step 1: Compute prefix sums
+        vector<int> prefix(nums.size());
+        prefix[0] = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+        vector<int> ans;
+        // Step 2: Binary search for each query
+        for (int q : queries) {
+            int l = 0, r = prefix.size();  // search range
+            while (l < r) {
+                int mid = l + (r - l) / 2;
+                if (prefix[mid] <= q) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
                 }
             }
-            ans.push_back(count);
+            ans.push_back(l); // 'l' is the count of elements whose sum <= q
         }
         return ans;
     }
