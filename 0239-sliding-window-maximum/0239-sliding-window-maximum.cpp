aaffcc4346c70d18e904694d05d3806d48dba394
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-07-09 10:18:45 +0530

    Time: 54 ms (22.32%), Space: 153.2 MB (15.24%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
-   vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-    deque<int> d; // to store indices of elements in current window
-    vector<int> ans; // result to store maximums
-    
-    int n = nums.size();
-    
-    // Process the first k elements (the first window)
-    for (int i = 0; i < k; i++) {
-        while (!d.empty() && nums[i] >= nums[d.back()]) {
-            d.pop_back();
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> pq;
+        vector<int> ans;
+        int start = 0;
+        int end = 0;
+
+        while (end < nums.size()) {
+           
+            pq.push({nums[end], end});
+
+      
+            if (end - start + 1 == k) {
+              
+                while (!pq.empty() && pq.top().second < start) {
+                    pq.pop();
+                }
+
+               
+                ans.push_back(pq.top().first);
+
+            
+                start++;
+            }
+
+           
+            end++;
         }
-        d.push_back(i);
+
+        return ans;
     }
-    
-    // Process the rest of the elements
-    for (int i = k; i < n; i++) {
-        ans.push_back(nums[d.front()]); // the front of deque is the maximum for the current window
-
-        // Remove elements not part of the window
-        while (!d.empty() && d.front() <= i - k) {
-            d.pop_front();
-        }
-
-        // Remove smaller elements in deque
-        while (!d.empty() && nums[i] >= nums[d.back()]) {
-            d.pop_back();
-        }
-        
-        // Add current element at the back of the deque
-        d.push_back(i);
-    }
-
-    // Add the max for the last window
-    ans.push_back(nums[d.front()]);
-    
-    return ans;
-}
-
 };
