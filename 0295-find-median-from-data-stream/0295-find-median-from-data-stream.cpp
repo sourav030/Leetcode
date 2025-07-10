29278715458db33a15699caf36430ff7503f0421
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-07-10 07:15:14 +0530

    Time: 51 ms (76.52%), Space: 125.1 MB (15.01%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,32 +1,42 @@
 class MedianFinder {
 public:
-    priority_queue<int> maxi; // Max heap for smaller half
-    priority_queue<int, vector<int>, greater<int>> mini; // Min heap for larger half
-
-    MedianFinder() {}
-
+    priority_queue<int>maxpq;
+    priority_queue<int,vector<int>, greater<int>>minpq;
+    MedianFinder() {
+        
+    }
+    
     void addNum(int num) {
-        if (maxi.empty() || num <= maxi.top()) {
-            maxi.push(num);
-        } else {
-            mini.push(num);
+        if(maxpq.empty() || num<maxpq.top()){
+            maxpq.push(num);
+        }
+        else{
+            minpq.push(num);
         }
 
-        // Balance the sizes
-        if (maxi.size() > mini.size() + 1) {
-            mini.push(maxi.top());
-            maxi.pop();
-        } else if (mini.size() > maxi.size()) {
-            maxi.push(mini.top());
-            mini.pop();
+        if(maxpq.size()>minpq.size()+1){
+            minpq.push(maxpq.top());
+            maxpq.pop();
+        }
+        if(minpq.size()>maxpq.size()){
+            maxpq.push(minpq.top());
+            minpq.pop();
         }
     }
-
+    
     double findMedian() {
-        if (maxi.size() == mini.size()) {
-            return (maxi.top() + mini.top()) / 2.0;
-        } else {
-            return maxi.top(); 
+        if(maxpq.size()>minpq.size()){
+            return maxpq.top();
+        }
+        else{
+            return (maxpq.top()+minpq.top())/2.0;
         }
     }
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
