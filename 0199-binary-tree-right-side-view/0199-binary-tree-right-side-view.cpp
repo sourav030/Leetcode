717a7520470a7227401853c63ec816b30e5b2cfd
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-07-24 11:13:01 +0530

    Time: 0 ms (100%), Space: 14.7 MB (94.88%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,18 +11,24 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int>&ans, int idx){
-        if(!root) return;
-        if(ans.size()==idx){
+    void solve(TreeNode* root, vector<int>&ans, int level){
+        if(!root){
+            return;
+        }
+        if(level==ans.size()){
             ans.push_back(root->val);
         }
-        solve(root->right,ans,idx+1);
-        solve(root->left,ans,idx+1);
+        if(root->right){
+            solve(root->right,ans,level+1);
+        }
+        if(root->left){
+            solve(root->left,ans,level+1);
+        }
     }
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
-        int idx=0;
-        solve(root,ans,idx);
+        int level=0;
+        solve(root,ans,level);
         return ans;
     }
 };
