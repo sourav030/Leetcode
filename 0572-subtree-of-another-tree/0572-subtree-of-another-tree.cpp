b3a3ad539dc42a928c78386c5c3cc7c780cdc8d0
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-07-24 22:28:01 +0530

    Time: 5 ms (8.48%), Space: 28.8 MB (92.94%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,20 +11,29 @@
  */
 class Solution {
 public:
-    bool isIdentical(TreeNode* root1,TreeNode* root2){
-        if(!root1 and !root2) return true;
-        if(!root1 or !root2) return false;
-        if(root1->val!=root2->val) return false;
-       return isIdentical(root1->left,root2->left) and isIdentical(root1->right,root2->right);
+    bool solve(TreeNode* root1, TreeNode* root2){
+        if(!root1 and !root2){
+            return true;
+        }
+        if(!root1 or !root2){
+            return false;
+        }
+        if(root1->val!=root2->val){
+            return false;
+        }
+        return solve(root1->left,root2->left) and solve(root1->right,root2->right);
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(!subRoot) return true;
-        if(!root) return false;
+        if(!root){
+            return false;
+        }
         if(root->val==subRoot->val){
-            if(isIdentical(root,subRoot)){
+            if(solve(root,subRoot)){
                 return true;
             }
         }
-        return isSubtree(root->left,subRoot) or isSubtree(root->right,subRoot);
+        bool left=isSubtree(root->left,subRoot);
+        bool right=isSubtree(root->right,subRoot);
+        return left or right;
     }
 };
