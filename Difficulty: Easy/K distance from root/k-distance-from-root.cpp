e76821284692c2b5d56e2490704f29f1fa5d229e
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-07-25 20:10:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/K distance from root/k-distance-from-root.cpp
@@ -0,0 +1,38 @@
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child /
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+
+class Solution {
+  public:
+    // function should print the nodes at k distance from root
+    vector<int> Kdistance(Node *root, int k) {
+        // Your code here
+        vector<int>ans;
+        int level=0;
+        queue<Node*>q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0; i<n; i++){
+                Node* node=q.front();
+                q.pop();
+                if(level==k){
+                    ans.push_back(node->data);
+                }
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
+            }
+            level++;
+        }
+        return ans;
+    }
+};
