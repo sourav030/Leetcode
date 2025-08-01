e184c5dc298d672c1b10b9ee9a6e49e156439192
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-08-01 13:06:53 +0530

    Time: 15 ms (34.56%), Space: 38.7 MB (25.63%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int, int> prefixSumFreq;
-        int sum = 0, count = 0;
-        
-        // Initialize prefix sum zero
-        prefixSumFreq[0] = 1;
-        
-        for (int num : nums) {
-            sum += num;  // Current prefix sum
-            
-            // Find subarrays with sum exactly equal to goal
-            if (prefixSumFreq.find(sum - goal) != prefixSumFreq.end()) {
-                count += prefixSumFreq[sum - goal];
+        unordered_map<int,int>mp;
+        int sum=0;
+        int ans=0;
+        for(int i=0; i<nums.size(); i++){
+            sum+=nums[i];
+            if(sum==goal){
+                ans++;
             }
-            
-            // Store current prefix sum count
-            prefixSumFreq[sum]++;
+            if(mp.find(sum-goal)!=mp.end()){
+                ans+=mp[sum-goal];
+            }
+            mp[sum]++;
         }
-        
-        return count;
+        return ans;
     }
 };
