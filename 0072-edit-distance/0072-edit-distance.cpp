84b19d5afda08a66e32f3f1e2aa8f86e0d3fb9fb
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-08-05 15:16:22 +0530

    Time: 10 ms (34.32%), Space: 13.2 MB (34.92%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -27,7 +27,23 @@ public:
     int minDistance(string word1, string word2) {
         int n=word1.length();
         int m=word2.length();
-        dp.resize(n+1,vector<int>(m+1,-1));
-        return solve(n,m,word1,word2);
+        // dp.resize(n+1,vector<int>(m+1,-1));
+        // return solve(n,m,word1,word2);
+        // agar hum isko ko bottom up main convert kre
+        vector<vector<int>>arr(n+1,vector<int>(m+1,0));
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i==0 or j==0){
+                    arr[i][j]=i+j;
+                }
+                else if(word1[i-1]==word2[j-1]){
+                    arr[i][j]=arr[i-1][j-1];
+                }
+                else{
+                    arr[i][j]=1+min({arr[i-1][j-1],arr[i-1][j],arr[i][j-1]});
+                }
+            }
+        }
+        return arr[n][m];
     }
 };
