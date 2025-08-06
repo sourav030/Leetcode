f5b4ab35b991605bb07fa070a3ea5228b56cd8b5
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-08-06 15:29:30 +0530

    Time: 51 ms (55.56%), Space: 79.9 MB (28.43%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    vector<vector<string>>ans;
-    vector<string>path;
-    void solve(int index,string s){
-        if(index==s.length()){
-            ans.push_back(path);
+    vector<vector<string>> ans;
+    vector<string> arr;
+    
+    void solve(int index, const string &str) {
+        if (index >= str.length()) {
+            ans.push_back(arr);
             return;
         }
-        for(int i=index; i<s.length(); i++){
-            if(isPalindrome(index,i,s)){
-                path.push_back(s.substr(index,i-index+1));
-                solve(i+1,s);
-                path.pop_back();
+        
+        for (int i = index; i < str.length(); i++) {
+            if (isPalindrome(index, i, str)) {  // pass end index, not length
+                arr.push_back(str.substr(index, i - index + 1));
+                solve(i + 1, str);
+                arr.pop_back();
             }
         }
     }
-
-    bool isPalindrome(int start, int end, string s){
-        while(start<=end){
-            if(s[start]!=s[end]){
-                return false;
-            }
+    
+    bool isPalindrome(int start, int end, const string &str) {
+        while (start < end) {
+            if (str[start] != str[end]) return false;
             start++;
             end--;
         }
         return true;
     }
+    
     vector<vector<string>> partition(string s) {
-     solve(0,s);
-     return ans;   
+        solve(0, s);
+        return ans;
     }
 };
