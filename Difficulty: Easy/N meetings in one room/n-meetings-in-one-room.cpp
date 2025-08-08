346f4f1fe38e52dc4249ea1142f33f5cc7436bb5
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-08-08 17:05:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -1,9 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     // Function to find the maximum number of meetings that can
@@ -31,37 +25,3 @@ class Solution {
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        string input;
-        int num;
-        vector<int> start;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            start.push_back(num);
-        }
-
-        vector<int> end;
-        getline(cin, input);
-        stringstream s22(input);
-        while (s22 >> num) {
-            end.push_back(num);
-        }
-
-        Solution ob;
-        int ans = ob.maxMeetings(start, end);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
