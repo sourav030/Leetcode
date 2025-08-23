cf28c337b5c8891629ce0727d2f8e3ac3008aec5
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-08-23 23:46:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.cpp
+++ Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.cpp
@@ -1,153 +1,39 @@
-//{ Driver Code Starts
-//Initial Template for C++
+/*
 
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// Tree Node
-struct Node {
+class Node {
+public:
     int data;
     Node* left;
     Node* right;
-};
 
-// Utility function to create a new Tree Node
-Node* newNode(int val) {
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-
-    return temp;
-}
-
-
-// Function to Build Tree
-Node* buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N') return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;) ip.push_back(str);
-
-    // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size()) break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
-
-/*Complete the function below
-
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){80
-        data = x;
-        left = right = NULL;
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
     }
 };
+
 */
 
-class Solution{
-    public:
-    //Function to check whether all nodes of a tree have the value 
-    //equal to the sum of their child nodes.
-    int solve(Node* root){
-        if(!root) return 1;
-        if(!root->left and !root->right) return 1;
-        int sum=0;
-        if(root->left){
-            sum+=root->left->data;
+class Solution {
+  public:
+    bool solve(Node* root){
+        if(!root){
+            return true;
         }
-        if(root->right){
-            sum+=root->right->data;
+        if(!root->left and !root->right){
+            return true;
         }
-        return root->data==sum and solve(root->left) and solve(root->right);
+        int val=root->left?root->left->data:0;
+        int val2=root->right?root->right->data:0;
+        if(root->data!=val+val2){
+            return false;
+        }
+        return solve(root->left) and solve(root->right);
     }
-    int isSumProperty(Node *root)
-    {
-     // Add your code here
-     return solve(root);
+    int isSumProperty(Node *root) {
+        // code here
+        return solve(root);
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-/* Driver program to test size function*/
-
-  
-
-int main() {
-
-   
-    int t;
-    scanf("%d ", &t);
-    while (t--) {
-        string s, ch;
-        getline(cin, s);
-        
-        Node* root = buildTree(s);
-        Solution ob;
-        cout << ob.isSumProperty(root) << endl;
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-
-// } Driver Code Ends
