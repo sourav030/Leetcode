20fcf379b059157d4b89b7d7234cbb0844e306ad
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-08-24 12:10:09 +0530

    Time: 3 ms (51.97%), Space: 9.5 MB (98.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,21 +4,8 @@ public:
         if(s.length()!=t.length()){
             return false;
         }
-        unordered_map<char,int>mp;
-        for(auto ele:s){
-            mp[ele]++;
-        }
-        for(auto ele:t){
-            if(mp.find(ele)!=mp.end()){
-                mp[ele]--;
-                if(mp[ele]==0){
-                    mp.erase(ele);
-                }
-            }
-            else{
-                return false;
-            }
-        }
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(), t.end());
+        return s==t;
     }
 };
