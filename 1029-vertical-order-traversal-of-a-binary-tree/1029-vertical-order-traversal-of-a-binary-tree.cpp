419e298e27c12238ff3a5adb22d355e07fc6a023
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-08-25 22:51:41 +0530

    Time: 2 ms (49.14%), Space: 16.7 MB (7.77%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,24 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    map<int, map<int, multiset<int>>> nodes;
-
-    void dfs(TreeNode* root, int row, int col) {
-        if (!root) return;
-        nodes[col][row].insert(root->val);
-        dfs(root->left, row + 1, col - 1);
-        dfs(root->right, row + 1, col + 1);
+    map<int,map<int,vector<int>> >mp;
+    void solve(TreeNode* root, int Xaxis, int height){
+        if(!root){
+            return ;
+        }
+        mp[Xaxis][height].push_back(root->val);
+        solve(root->left,Xaxis-1,height+1);
+        solve(root->right,Xaxis+1,height+1);
     }
-
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        dfs(root, 0, 0);  // root is at (row=0, col=0)
-        
-        vector<vector<int>> ans;
-        for (auto& [col, rowMap] : nodes) {
-            vector<int> colVals;
-            for (auto& [row, vals] : rowMap) {
-                colVals.insert(colVals.end(), vals.begin(), vals.end());
+        solve(root,0,0);
+        vector<vector<int>>ans;
+        for(auto ele:mp){
+           vector<int> col;
+            for (auto &ele2 : ele.second) {
+                auto temp = ele2.second;
+                sort(temp.begin(), temp.end());
+                col.insert(col.end(), temp.begin(), temp.end());
             }
-            ans.push_back(colVals);
+            ans.push_back(col);   // ✅ yaha hona chahiye
+
         }
         return ans;
     }
