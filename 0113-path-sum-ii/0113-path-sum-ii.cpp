f93063bfdfb1da75e1f70974b017913e7296853d
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-08-25 16:02:37 +0530

    Time: 0 ms (100%), Space: 21 MB (46.34%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -1,36 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void solve(TreeNode* root, int targetSum, vector<int>path){
-        if(!root){
-            return;
-        }
-        if(!root->left and !root->right){
-            int val=targetSum-root->val;
-            path.push_back(root->val);
-            if(val==0){
+    vector<vector<int>> ans;
+
+    void solve(TreeNode* root, int targetSum, vector<int>& path) {
+        if (!root) return;
+
+        path.push_back(root->val);
+
+        if (!root->left && !root->right) { // leaf node
+            if (targetSum == root->val) {
                 ans.push_back(path);
             }
-            return;
+        } else {
+            solve(root->left, targetSum - root->val, path);
+            solve(root->right, targetSum - root->val, path);
         }
-        path.push_back(root->val);
-        solve(root->left, targetSum-root->val,path);
-        solve(root->right,targetSum-root->val,path);
+
+        path.pop_back(); // backtrack
     }
+
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<int>path;
-        solve(root,targetSum,path);
+        vector<int> path;
+        solve(root, targetSum, path);
         return ans;
     }
 };
