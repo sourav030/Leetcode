f21ebd3558dc13bc449ad46a8d0eabd5cbce7094
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-09-04 21:20:03 +0530

    Time: 107 ms (19.3%), Space: 131.2 MB (18.25%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>>result;
-    void solve(int index,int k,int n, vector<int> &combination){
-        if(combination.size()==k) result.push_back(combination);
+    vector<vector<int>> ans;
 
-        for(int i=index; i<=n;i++){
-            combination.push_back(i);
-            solve(i+1,k,n,combination);
-            combination.pop_back();
+    void solve(vector<int>& arr, int index, int n, int k) {
+        if (k == 0) {
+            ans.push_back(arr);  // store the current combination
+            return;
         }
+        if (index > n) {
+            return;
+        }
+
+        // Choose current number
+        arr.push_back(index);
+        solve(arr, index + 1, n, k - 1);
+        arr.pop_back();
+
+        // Skip current number
+        solve(arr, index + 1, n, k);
     }
+
     vector<vector<int>> combine(int n, int k) {
-        vector<int>combination;
-        solve(1,k,n,combination);
-        return result;
+        vector<int> nums;
+        solve(nums, 1, n, k);
+        return ans;
     }
 };
