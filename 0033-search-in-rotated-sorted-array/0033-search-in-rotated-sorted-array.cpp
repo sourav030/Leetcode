e3ebdff88cbacb2c506e228694b64bba51a3219b
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-09-11 17:55:16 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.01%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;
+        int start=0;
+        int end=nums.size()-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[start]<=nums[mid]){
+                // Then Two case if formed
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) return mid;
-
-            // Left half is sorted
-            if (nums[start] <= nums[mid]) {
-                if (target >= nums[start] && target < nums[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
+                //1 answer exist our not
+                if(nums[start]<=target and target<nums[mid]){
+                    end=mid-1;
+                }
+                // answer not exist in this part
+                else{
+                    start=mid+1;
                 }
             }
-            // Right half is sorted
-            else {
-                if (target > nums[mid] && target <= nums[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
+            else{
+                // two case answer exist our not
+                if(nums[mid]<target and target<=nums[end]){
+                    start=mid+1;
+                }
+                else{
+                    end=mid-1;
                 }
             }
         }
-
         return -1;
     }
 };
