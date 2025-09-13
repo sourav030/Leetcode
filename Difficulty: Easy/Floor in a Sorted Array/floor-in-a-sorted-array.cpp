567a2bbe9b650794907fd1724bb5cf5761cf409c
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-09-13 23:36:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
@@ -1,56 +1,21 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-  public:
+public:
+    int findFloor(vector<int>& arr, int x) {
+        int start = 0, end = arr.size() - 1;
+        int ans = -1;
 
-    int findFloor(vector<int>& arr, int k) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-        // Your code here
-        int start=0;
-        int end=arr.size()-1;
-        int ans=-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(k<arr[mid]){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
-                ans=mid;
+          
+            if (arr[mid] <= x) {
+                ans = mid;      // candidate floor
+                start = mid + 1;
+            } 
+            else {
+                end = mid - 1;  // too big, go left
             }
         }
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution ob;
-        int ans = ob.findFloor(arr, k);
-
-        cout << ans << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
