e6f1bfb87ad512138cf9d1d4bdfcd873cb3aa446
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-09-13 10:22:56 +0530

    Time: 15 ms (14.19%), Space: 145 MB (9.42%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>postive;
-        vector<int>negative;
+        vector<int>Positive;
+        vector<int>Negative;
+        vector<int>Ans;
         for(int i=0; i<nums.size(); i++){
+            if(nums[i]>0){
+                Positive.push_back(nums[i]);
+            }
             if(nums[i]<0){
-                negative.push_back(nums[i]);
-            }
-            else{
-                postive.push_back(nums[i]);
+                Negative.push_back(nums[i]);
             }
         }
-        vector<int>result;
-        int n=postive.size();
-        int m=negative.size();
-        bool chance=0;
-        int i=0;
+        bool pos=1;
+        int i=0; 
         int j=0;
-        while(i<n and j<m){
-            if(chance==0){
-                result.push_back(postive[i]);
+        while(i<Positive.size() and j<Negative.size()){
+            if(pos){
+                Ans.push_back(Positive[i]);
                 i++;
-                chance=1;
             }
             else{
-                result.push_back(negative[j]);
+                Ans.push_back(Negative[j]);
                 j++;
-                chance=0;
-
             }
+            pos=!pos;
         }
-        while(i<n){
-            result.push_back(postive[i]);
+        while(i<Positive.size()){
+            Ans.push_back(Positive[i]);
             i++;
         }
-        while(j<m){
-            result.push_back(negative[j]);
-                j++;
+        while(j<Negative.size()){
+            Ans.push_back(Negative[j]);
+            j++;
         }
-        return result;
+        return Ans;
     }
 };
