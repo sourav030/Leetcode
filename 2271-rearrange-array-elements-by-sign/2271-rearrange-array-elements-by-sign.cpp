2678659a8594df19f789ef7e85e4e54c7ecaece2
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-09-13 10:24:10 +0530

    Time: 5 ms (69.31%), Space: 128 MB (99.77%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>Positive;
-        vector<int>Negative;
-        vector<int>Ans;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]>0){
-                Positive.push_back(nums[i]);
-            }
-            if(nums[i]<0){
-                Negative.push_back(nums[i]);
+        int n = nums.size();
+        vector<int> ans(n);
+        int pos = 0, neg = 1;  // positive ko even index, negative ko odd index
+
+        for (int x : nums) {
+            if (x > 0) {
+                ans[pos] = x;
+                pos += 2;
+            } else {
+                ans[neg] = x;
+                neg += 2;
             }
         }
-        bool pos=1;
-        int i=0; 
-        int j=0;
-        while(i<Positive.size() and j<Negative.size()){
-            if(pos){
-                Ans.push_back(Positive[i]);
-                i++;
-            }
-            else{
-                Ans.push_back(Negative[j]);
-                j++;
-            }
-            pos=!pos;
-        }
-        while(i<Positive.size()){
-            Ans.push_back(Positive[i]);
-            i++;
-        }
-        while(j<Negative.size()){
-            Ans.push_back(Negative[j]);
-            j++;
-        }
-        return Ans;
+        return ans;
     }
 };
