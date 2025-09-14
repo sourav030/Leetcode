e2f3d25d2ef8f4572663b6f92409c7993f22be78
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-09-14 10:36:10 +0530

    Time: 19 ms (7.1%), Space: 8.6 MB (50.69%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return int(sqrt(x));
+        int ans=1;
+        if(x==0){
+            return 0;
+        }
+        for(int i=1; i<x; i++){
+            if(pow(i,2)<=x){
+                ans=i;
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
     }
 };
