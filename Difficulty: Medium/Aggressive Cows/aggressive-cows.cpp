aaa5c2a498506414272a3767d36f856893010115
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-09-18 12:20:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -1,41 +1,26 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
-
 class Solution {
   public:
-   bool isPossible(vector<int> &arr, int k, int mid){
-       int cow=1;
-       int position=arr[0];
-       for(int i=1; i<arr.size(); i++){
-           if(arr[i]-position>=mid){
-               cow++;
-               position=arr[i];
-           }
-           
-       }
-       return cow>=k;
-   }
-
+    bool solve(vector<int>&stalls, int k, int dis){
+        int count=1;
+        int prev=stalls[0];
+        for(int i=1; i<stalls.size(); i++){
+            if(stalls[i]>=prev+dis){
+                count++;
+                prev=stalls[i];
+            }
+        }
+        return count>=k;
+    }
     int aggressiveCows(vector<int> &stalls, int k) {
-
-        // Write your code here
+        // code here
         int n=stalls.size();
         sort(stalls.begin(),stalls.end());
-        if (k > n) return -1; // Invalid input
-
         int low=1;
-        int high=stalls[n-1]-stalls[0];
-        int ans=0;
+        int high = stalls[n-1] - stalls[0];
+        int ans=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(isPossible(stalls,k,mid)){
+            if(solve(stalls, k, mid)){
                 ans=mid;
                 low=mid+1;
             }
@@ -44,39 +29,6 @@ class Solution {
             }
         }
         return ans;
+        
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int k;
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string in;
-        getline(cin, in);
-        stringstream sss(in);
-        int num;
-        while (sss >> num) {
-            k = num;
-        }
-        Solution ob;
-        int ans = ob.aggressiveCows(arr, k);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
