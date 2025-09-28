6ae2fd92f42fd1ad9efaed4152277768cf682324
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-09-28 12:44:14 +0530

    Time: 29 ms (38.51%), Space: 139.1 MB (59.04%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-         priority_queue<pair<int,int>>pq;
-         int end=0;
-         int start=0;
-         vector<int>ans;
-         while(end<nums.size()){
-            pq.push({nums[end],end});
-            if(end-start+1==k){
-                while(!pq.empty() and pq.top().second<start){
-                    pq.pop();
-                }
-                start++;
-                ans.push_back(pq.top().first);
-            }
-            end++;
-         }
-         return ans;
+        deque<int> dq;  // stores indices
+        vector<int> ans;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            // remove indices out of window
+            while(!dq.empty() && dq.front() <= i - k)
+                dq.pop_front();
+            
+            // remove smaller elements from back
+            while(!dq.empty() && nums[dq.back()] < nums[i])
+                dq.pop_back();
+            
+            dq.push_back(i);
+            
+            // window size >= k → record max
+            if(i >= k - 1)
+                ans.push_back(nums[dq.front()]);
+        }
+        
+        return ans;
     }
 };
