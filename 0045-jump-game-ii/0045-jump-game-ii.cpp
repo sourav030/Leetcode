fb94b0f9dd038f0a50975f2a34a40638174e6fad
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-09-28 12:24:31 +0530

    Time: 231 ms (5.46%), Space: 22.5 MB (10.33%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    int solve(vector<int>&nums, int idx, vector<int>&dp){
-        if(idx>=nums.size()-1) return 0;
-        if(nums[idx]==0) return INT_MAX;
-        if(dp[idx]!=-1) return dp[idx]; 
-        int res=INT_MAX;
+    vector<int>dp;
+    int solve(vector<int>&nums, int idx , int n){
+        if(idx>=n-1){
+            return 0;
+        }
+        if(nums[idx]==0){
+            return INT_MAX;
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int jump=INT_MAX;
         for(int i=1; i<=nums[idx]; i++){
-            int tem=solve(nums,idx+i, dp);
-            if(tem!=INT_MAX){
-                res=min(res,tem+1);
+            int val=solve(nums, idx+i,n);
+            if(val!=INT_MAX){
+                jump=min(jump,val+1);
             }
         }
-        return dp[idx]=res;
+        return dp[idx]=jump;
     }
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        return solve(nums,0, dp);
+        dp.resize(n+1,-1);
+        dp.resize(n+1,-1);
+        return solve(nums, 0, n);
     }
 };
