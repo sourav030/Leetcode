b824f3c2d2d1382cd23fa59f150130af6da6a99f
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-10-24 23:20:08 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int solve(vector<int>&dp, int n){
-        if(n==0 || n==1){
+    vector<int>dp;
+    int solve(int n){
+        if(n<=0 or n==1){
             return 1;
         }
         if(dp[n]!=-1){
             return dp[n];
         }
-        return dp[n]=solve(dp,n-1)+solve(dp,n-2);
+        return dp[n]=solve(n-1)+solve(n-2);
     }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(dp,n);
+        dp.resize(n+1,-1);
+       return solve(n);
     }
 };
