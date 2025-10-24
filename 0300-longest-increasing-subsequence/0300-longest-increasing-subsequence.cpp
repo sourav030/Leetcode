aa4fc6f627abca2830c1c95b8c9f459d98029031
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-10-24 23:36:49 +0530

    Time: 635 ms (5.05%), Space: 296.3 MB (28.71%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int index, int n, int prev, vector<vector<int>>&dp){
-        if(index>=n)return 0;
-        if(dp[index][prev+1]!=-1) return dp[index][prev+1];
-        int take=0;
-        int notTake=0;
-        if(prev==-1 or nums[index]>nums[prev]){
-            take=1+solve(nums,index+1,n,index,dp);
-        }
-        notTake=0+solve(nums,index+1,n,prev,dp);
-        return dp[index][prev+1]= max(notTake,take);
-    }
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(nums,0,n,-1,dp);
+    vector<vector<int>> dp;
 
+    int solve(vector<int>& nums, int index, int prev) {
+        if (index >= nums.size()) {
+            return 0;
+        }
+
+        // Shift prev by +1 to avoid using -1 as an index
+        if (dp[index][prev + 1] != -1) {
+            return dp[index][prev + 1];
+        }
+
+        int take = INT_MIN;
+        if (prev == -1 || nums[index] > nums[prev]) {
+            take = 1 + solve(nums, index + 1, index);
+        }
+
+        int nottake = solve(nums, index + 1, prev);
+
+        return dp[index][prev + 1] = max(take, nottake);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        dp.assign(n, vector<int>(n + 1, -1));  // +1 for the case prev = -1
+        return solve(nums, 0, -1);
     }
 };
