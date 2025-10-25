8b2c526ac0b6236f2f8d8f651f15bdcae198a539
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-10-25 12:39:47 +0530

    Time: 4 ms (68.72%), Space: 23.8 MB (81.01%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>>ans;
         ans.push_back(intervals[0]);
-        int i=1;
-        while(i<intervals.size()){
+        for(int i=0; i<intervals.size(); i++){
             vector<int>&a=ans.back();
-            // now at this we chek that endtime off this interval is greater our not
-            // start time of another interval
             if(a[1]>=intervals[i][0]){
                 a[0]=min(a[0],intervals[i][0]);
                 a[1]=max(a[1],intervals[i][1]);
@@ -16,7 +13,6 @@ public:
             else{
                 ans.push_back(intervals[i]);
             }
-            i++;
         }
         return ans;
     }
