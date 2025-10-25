979fef57ee6c15b802ac267868bf6e30b21b10bf
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-10-25 12:26:56 +0530

    Time: 0 ms (100%), Space: 15.6 MB (92.36%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int pivot=-1;
+        int pivotIndex=-1;
         for(int i=nums.size()-2; i>=0; i--){
             if(nums[i]<nums[i+1]){
-                pivot=i;
+                pivotIndex=i;
                 break;
             }
         }
-        if(pivot==-1){
-            reverse(nums.begin(), nums.end());
-            return ;
+        if(pivotIndex==-1){
+            reverse(nums.begin(),nums.end());
+            return;
         }
-        for(int i=nums.size()-1; i>=0; i--){
-            if(nums[i]>nums[pivot]){
-                swap(nums[i],nums[pivot]);
+        for(int i=nums.size()-1; i>pivotIndex; i--){
+            if(nums[i]>nums[pivotIndex]){
+                swap(nums[i],nums[pivotIndex]);
                 break;
             }
         }
-        reverse(nums.begin()+pivot+1,nums.end());
+
+        reverse(nums.begin()+pivotIndex+1,nums.end());
+        return;
     }
 };
