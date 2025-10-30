82552721e5573774ce4faf9a2de83b2dfb501794
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-10-30 21:26:58 +0530

    Time: 1 ms (76.89%), Space: 10.9 MB (69.54%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-         unordered_map<char, int> freq;
-    int left = 0, maxFreq = 0, maxLen = 0;
-    
-    for (int right = 0; right < s.length(); right++) {
-        freq[s[right]]++;
-        maxFreq = max(maxFreq, freq[s[right]]);
-        
-        while ((right - left + 1) - maxFreq > k) {
-            freq[s[left]]--;
-            left++;
+        vector<int> freq(26, 0);
+        int left = 0, right = 0;
+        int maxFreq = 0;  // highest frequency char in window
+        int maxLen = 0;
+
+        while (right < s.size()) {
+            freq[s[right] - 'A']++;
+            maxFreq = max(maxFreq, freq[s[right] - 'A']);
+
+            // If window is invalid → shrink it
+            while ((right - left + 1) - maxFreq > k) {
+                freq[s[left] - 'A']--;
+                left++;
+            }
+
+            maxLen = max(maxLen, right - left + 1);
+            right++;
         }
-        
-        maxLen = max(maxLen, right - left + 1);
-    }
-    
-    return maxLen;
+
+        return maxLen;
     }
 };
