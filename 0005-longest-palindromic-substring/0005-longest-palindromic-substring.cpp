e800c022dd02b34e5302bd0187e606923bd6fa42
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-10-30 19:14:48 +0530

    Time: 419 ms (13.51%), Space: 291.9 MB (8.62%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int Solve(string &str, int start, int end){
+    int palindrome(int start, int end, string &s){
         if(start>=end){
-            return true;
+            return 1;
         }
         if(dp[start][end]!=-1){
             return dp[start][end];
         }
-        if(str[start]==str[end]){
-            return dp[start][end]= Solve(str,start+1,end-1);
+        if(s[start]==s[end]){
+            dp[start][end]=palindrome(start+1,end-1,s);
         }
-        return dp[start][end]= false;
+        else{
+            dp[start][end]=0;
+        }
+        return dp[start][end];
     }
     string longestPalindrome(string s) {
-        int start=0;
-        int len=0;
         int n=s.length();
-        dp.resize(n+1,vector<int>(n+1,-1));
+        dp.assign(n, vector<int>(n, -1));
 
-        for(int i=0; i<s.length(); i++){
-            for(int j=i; j<s.length(); j++){
-                if(Solve(s,i,j)){
-                    if(j-i+1>len){
-                        start=i;
-                        len=j-i+1;
-                    }
+        int start=0;
+        int end=0;
+
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(palindrome(i,j,s) and (j-i+1)>(end-start+1)){
+                    start=i;
+                    end=j;
                 }
             }
         }
-
-        return s.substr(start,len);
+        return s.substr(start, end-start+1);
     }
 };
