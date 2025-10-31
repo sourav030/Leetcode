ebb03efaefc60fcd6ad7b5b343aa0dd1f41e2162
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-10-31 17:53:51 +0530

    Time: 19 ms (29.48%), Space: 14.3 MB (29.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,22 +5,18 @@ public:
         int start=0;
         int end=0;
         unordered_map<char,int>mp;
-
         while(end<s.length()){
             if(mp.find(s[end])==mp.end()){
-                mp[s[end]]=1;
+                mp[s[end]]++;
             }
             else{
-                while(mp.find(s[end])!=mp.end() and start<end){
+                while(start<end and mp.find(s[end])!=mp.end()){
                     mp.erase(s[start]);
                     start++;
                 }
-                mp[s[end]]=1;
+                mp[s[end]]++;
             }
-            
-            if(mp.size()>ans){
-                ans=mp.size();
-            }
+            ans=max(ans,end-start+1);
             end++;
         }
         return ans;
