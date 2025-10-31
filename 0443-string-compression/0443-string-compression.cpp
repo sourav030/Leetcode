19d7ce754f2779c2aab1c69493f0de899fdcb984
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-10-31 17:50:14 +0530

    Time: 0 ms (100%), Space: 13.8 MB (44.71%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int index=0;
-        int i=0; 
-        while(i<chars.size()){
-            char current=chars[i];
-            int count=1;
-            i++;
-            while(i<chars.size() and chars[i]==current){
+        int index = 0;  // write pointer
+        for (int i = 0; i < chars.size(); i++) {
+            char ch = chars[i];
+            int count = 0;
+
+           
+            while (i < chars.size() && chars[i] == ch) {
                 i++;
                 count++;
             }
-            chars[index++]=current;
-            if(count>1){
-                string str=to_string(count);
-                for(auto ele:str){
-                    chars[index++]=ele;
-                }
+
+     
+            chars[index++] = ch;
+
+           
+            if (count > 1) {
+                string str = to_string(count);
+                for (char c : str)
+                    chars[index++] = c;
             }
+
+            i--; 
         }
         return index;
     }
