a7b232bb834f589073eebc3e87351f2d5c5518db
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-11-01 17:47:48 +0530

    Time: 0 ms (100%), Space: 9.2 MB (54.95%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0;
-        int n = s.length();
-        long ans = 0;     // use long to detect overflow
+        int i = 0, n = s.length();
+        while (i < n && s[i] == ' ') i++;  
+
         int sign = 1;
-
-        // 1. Skip leading spaces
-        while (i < n && s[i] == ' ') {
-            i++;
-        }
-
-        // 2. Handle sign
         if (i < n && (s[i] == '+' || s[i] == '-')) {
-            if (s[i] == '-') sign = -1;
+            sign = (s[i] == '-') ? -1 : 1;
             i++;
         }
 
-        // 3. Process digits
+        long result = 0; 
+
         while (i < n && isdigit(s[i])) {
             int val = s[i] - '0';
-            ans = ans * 10 + val;
 
-            // 4. Check overflow
-            if (sign == 1 && ans > INT_MAX) return INT_MAX;
-            if (sign == -1 && -ans < INT_MIN) return INT_MIN;
+        
+            if (result > (INT_MAX - val) / 10) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
 
+            result = result * 10 + val;
             i++;
         }
 
-        return sign * ans;
+        return (int)(result * sign);
     }
 };
