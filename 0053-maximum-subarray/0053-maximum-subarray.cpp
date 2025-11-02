377088781f67243d9fc72345141b35c12143ed9d
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-11-02 13:19:34 +0530

    Time: 0 ms (100%), Space: 71.7 MB (80.46%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,9 +4,11 @@ public:
         int ans=INT_MIN;
         int sum=0;
         for(int i=0; i<nums.size(); i++){
-            sum+=nums[i];
-            sum=max(sum,nums[i]);
+            sum=max(sum+nums[i],nums[i]);
             ans=max(ans,sum);
+            if(sum<0){
+                sum=0;
+            }
         }
         return ans;
     }
