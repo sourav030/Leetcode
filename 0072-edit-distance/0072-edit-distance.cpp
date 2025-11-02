d60b0723e983356329779d89d2ffdde9cf071b9a
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-11-02 12:15:05 +0530

    Time: 62 ms (12.56%), Space: 85 MB (5.01%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int solve(int n, int m, string word1,string word2){
+    int solve(string word1,string word2, int n, int m){
         if(n==0){
             return m;
         }
@@ -12,38 +12,16 @@ public:
             return dp[n][m];
         }
         if(word1[n-1]==word2[m-1]){
-            return dp[n][m]=solve(n-1,m-1,word1,word2);
+            return dp[n][m]=solve(word1,word2,n-1,m-1);
         }
         else{
-            return dp[n][m]=1+min(
-                {
-                solve(n,m-1,word1,word2), // insert opertaion perform
-                solve(n-1,m,word1,word2), // delete operation perform
-                solve(n-1,m-1,word1,word2) // replace opertion perform
-            }
-            );
+            return dp[n][m]=1+min({solve(word1,word2,n-1,m),solve(word1,word2,n-1,m-1),solve(word1,word2,n,m-1)});
         }
     }
     int minDistance(string word1, string word2) {
         int n=word1.length();
         int m=word2.length();
-        // dp.resize(n+1,vector<int>(m+1,-1));
-        // return solve(n,m,word1,word2);
-        // agar hum isko ko bottom up main convert kre
-        vector<vector<int>>arr(n+1,vector<int>(m+1,0));
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=m; j++){
-                if(i==0 or j==0){
-                    arr[i][j]=i+j;
-                }
-                else if(word1[i-1]==word2[j-1]){
-                    arr[i][j]=arr[i-1][j-1];
-                }
-                else{
-                    arr[i][j]=1+min({arr[i-1][j-1],arr[i-1][j],arr[i][j-1]});
-                }
-            }
-        }
-        return arr[n][m];
+        dp.assign(n+1,vector<int>(m+1,-1));
+        return solve(word1,word2,n,m);
     }
 };
