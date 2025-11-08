0048f923e6bba0369218e81db0e543d2ddfddac7
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-11-08 14:33:59 +0530

    Time: 255 ms (60.26%), Space: 10.5 MB (72.27%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
-    vector<int> row = {-1, 1, 0, 0};
-    vector<int> col = {0, 0, 1, -1};
-    int n, m;
-
-    bool solve(vector<vector<char>>& matrix, int r, int c, string& word, int idx) {
-        if (idx == word.length()) return true;
-
-        char temp = matrix[r][c];
-        matrix[r][c] = '#';  
-
-        for (int i = 0; i < 4; i++) {
-            int new_r = r + row[i];
-            int new_c = c + col[i];
-
-            if (new_r >= 0 && new_r < n && new_c >= 0 && new_c < m &&
-                matrix[new_r][new_c] == word[idx]) {
-                if (solve(matrix, new_r, new_c, word, idx + 1)) {
-                    return true;
+    vector<int>row={0,0,-1,1};
+    vector<int>col={1,-1,0,0};
+    int n,m;
+    bool solve(vector<vector<char>>&board, int idx, string &word, int r, int c){
+        if(idx>=word.length()){
+            return true;
+        }
+        char ch=board[r][c];
+        board[r][c]='#';
+        for(int i=0; i<4; i++){
+            int nr=r+row[i];
+            int nc=c+col[i];
+            if(nr>=0 and nr<n and nc>=0 and nc<m){
+                if(board[nr][nc]==word[idx]){
+                    if(solve(board,idx+1,word,nr,nc)){
+                        return true;
+                    }
                 }
             }
         }
-
-        matrix[r][c] = temp;  
+        board[r][c]=ch;
         return false;
     }
-
     bool exist(vector<vector<char>>& board, string word) {
-        n = board.size();
-        m = board[0].size();
+        n=board.size();
+        m=board[0].size();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == word[0]) {
-                    if (solve(board, i, j, word, 1)) {
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j]==word[0]){
+                    if(solve(board,1,word,i,j)){
                         return true;
                     }
                 }
             }
         }
         return false;
+
     }
 };
