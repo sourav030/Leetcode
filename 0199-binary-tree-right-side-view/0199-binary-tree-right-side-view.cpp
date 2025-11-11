1f32d8c388aedd9e76e4ae76c39ba245d0ba36a1
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-11-11 08:50:10 +0530

    Time: 0 ms (100%), Space: 15 MB (61.1%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,29 +11,19 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
+    vector<int>ans;
+    void solve(TreeNode* root, int level){
         if(!root){
-            return {};
+            return;
         }
-        queue<TreeNode*>q;
-        q.push(root);
-        vector<int>ans;
-        while(!q.empty()){
-            int n=q.size(); // first question is why we getting the size of queue 
-            // reson we want to performa level order traversal and want first node of each level
-            ans.push_back(q.front()->val);
-            for(int i=0; i<n; i++){
-                TreeNode* node=q.front();
-                q.pop();
-                if(node->right){
-                    q.push(node->right);
-                }
-                if(node->left){
-                    q.push(node->left);
-                }
-            }
+        if(ans.size()==level){
+            ans.push_back(root->val);
         }
+        solve(root->right,level+1);
+        solve(root->left,level+1);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        solve(root,0);
         return ans;
-
     }
 };
