71fba2ab9c29cf7035c4b6d29e771194ee3b8b70
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-11-12 11:19:08 +0530

    Time: 152 ms (5.81%), Space: 35.6 MB (10.67%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
@@ -1,18 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int sum = 0;
-
-    void reverseInorder(TreeNode* root) {
-        if (!root) return;
-
-        reverseInorder(root->right);
-        sum += root->val;
-        root->val = sum;
-        reverseInorder(root->left);
+    vector<int>Inorder;
+    void solve(TreeNode* root){
+        if(!root) return;
+        solve(root->left);
+        Inorder.push_back(root->val);
+        solve(root->right);
     }
-
+    int find(int value){
+        int sum=0;
+        for(int i=0; i<Inorder.size(); i++){
+            if(Inorder[i]>=value){
+                sum+=Inorder[i];
+            }
+        }
+        return sum;
+    }
     TreeNode* convertBST(TreeNode* root) {
-        reverseInorder(root);
-        return root;
+        if(!root) return nullptr;
+        solve(root);
+        queue<TreeNode*>q;
+        TreeNode* curr=root;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* Node=q.front();
+            q.pop();
+            int val=find(Node->val);
+            Node->val=val;
+            if(Node->left){
+                q.push(Node->left);
+            }
+            if(Node->right){
+                q.push(Node->right);
+            }
+        }
+        return curr;
     }
 };
