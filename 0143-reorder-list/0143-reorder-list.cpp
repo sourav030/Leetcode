17f0a90bc83ca2f190b12111b844a4ead239e6ae
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-11-15 12:52:08 +0530

    Time: 19 ms (6.49%), Space: 27.4 MB (5.31%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -1,63 +1,46 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* solve(ListNode* head){
-        ListNode* fast=head;
-        ListNode* slow=head;
-        ListNode* prev=nullptr;
-        while(fast and fast->next){
-            prev=slow;
-            slow=slow->next;
-            fast=fast->next->next;
+    int length(ListNode* head) {
+        int len = 0;
+        while (head) {
+            len++;
+            head = head->next;
         }
-        prev->next=nullptr;
-        return slow;
+        return len;
     }
-    ListNode* reverse(ListNode* prev,ListNode* curr){
-        if(!curr){
-            return prev;
-        }
-        ListNode* next=curr->next;
-        curr->next=prev;
-        return reverse(curr,next);
-    }
+
     void reorderList(ListNode* head) {
-        if(!head or !head->next) return ;
-        ListNode* mid=solve(head);
-        ListNode* midReverse=reverse(nullptr,mid);
-        ListNode* curr=head;
-        head=head->next;
-        bool chance=true;
-        while(head and midReverse){
-                if(chance){
-                    curr->next=midReverse;
-                    midReverse=midReverse->next;
-                }
-                else{
-                    curr->next=head;
-                    head=head->next;
-                }
-                curr=curr->next;
-                chance=!chance;
+        if (!head || !head->next) return;
+
+        int len = length(head);
+
+     
+        map<int, ListNode*> mp;
+        ListNode* curr = head;
+        for (int i = 1; i <= len; i++) {
+            mp[i] = curr;
+            curr = curr->next;
         }
-        while(head){
-            curr->next=head;
-            head=head->next;
-            curr=curr->next;
+
+      
+        int left = 1, right = len;
+        curr = head;
+
+        while (left < right) {
+          
+            curr->next = mp[right];
+            curr = curr->next;
+            right--;
+
+          
+            if (left < right) {
+                curr->next = mp[left + 1];
+                curr = curr->next;
+                left++;
+            }
         }
-        while(midReverse){
-            curr->next=midReverse;
-            midReverse=midReverse->next;
-            curr=curr->next;
-        }
+
+        
+        curr->next = nullptr;
     }
 };
