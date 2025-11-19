915dcad927eb5bdafb10717dc987109b46bf2a98
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-11-19 09:54:14 +0530

    Time: 3 ms (9.55%), Space: 8.8 MB (66.12%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(int i=0; i<s.size(); i++)
-        {
-            if(s[i]=='(' or s[i]=='{' or s[i]=='[')
-            {
-                st.push(s[i]);
+        stack<char>st;
+        for(auto ele:s){
+            if(ele=='(' or ele=='[' or ele=='{'){
+                st.push(ele);
             }
-            else
-            { if(!st.empty()){
-               char x=st.top();
-               if(s[i]==')' and x=='(' and !st.empty())
-               {
-                   st.pop();
-               }
-               else if(s[i]==']' and x=='[' and !st.empty())
-               {
-                   st.pop();
-               }
-               else if(s[i]=='}' and x=='{' and !st.empty())
-               {
-                   st.pop();
-               }
-               else
-               {
-                   return false;
-               }
-
-            }else
-               {
-                   return false;
-               }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                else if(ele==')' and st.top()=='('){
+                    st.pop();
+                }
+                 else if(ele==']' and st.top()=='['){
+                    st.pop();
+                }
+                 else if(ele=='}' and st.top()=='{'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
             }
         }
-        if(st.empty())
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
+        return st.empty();
     }
 };
