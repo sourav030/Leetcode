9eb6df7bc5f585ff60eca9a5ee6bad45cbdb14d4
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-11-24 09:17:12 +0530

    Time: 592 ms (8.19%), Space: 55.7 MB (6.16%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     vector<int>dp;
-    int solve(vector<int>&nums, int idx, int n){
-        if(idx>=n-1){
-            return 1;
-        }
-        if(nums[idx]==0){
-            return 0;
+    int solve(int idx, vector<int>&nums){
+        if(idx>=nums.size()-1){
+            return true;
         }
         if(dp[idx]!=-1){
             return dp[idx];
         }
+        if(nums[idx]==0){
+            return false;
+        }
         for(int i=1; i<=nums[idx]; i++){
-            if(solve(nums, idx+i,n)){
-                return dp[idx]=1;
+            if(solve(idx+i,nums)){
+                return dp[idx]= 1;
             }
         }
         return dp[idx]=0;
     }
     bool canJump(vector<int>& nums) {
         int n=nums.size();
+        if(nums[0]==0 and n==1){
+            return true;
+        }
         dp.resize(n+1,-1);
-        int idx=0;
-        return solve(nums,idx,n);
+        return solve(0,nums);
     }
 };
