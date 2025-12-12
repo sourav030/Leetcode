36a5f6aadc9cd2a95b3a0cd7ff1ba1d2f724b6ed
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-12-12 18:16:50 +0530

    Time: 11 ms (11.27%), Space: 26.5 MB (42.11%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isPalindrome(int start, int end, string str){
+    bool check(int start, int end, string str){
         while(start<end){
             if(str[start]!=str[end]){
                 return false;
@@ -15,7 +15,12 @@ public:
         int end=s.length()-1;
         while(start<end){
             if(s[start]!=s[end]){
-                return isPalindrome(start+1,end,s) or isPalindrome(start,end-1,s);
+                if(check(start+1,end,s) or check(start,end-1,s)){
+                    return true;
+                }
+                else{
+                    return false;
+                }
             }
             start++;
             end--;
