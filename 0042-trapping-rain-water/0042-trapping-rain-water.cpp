5e5307e54bcf6b5c1ab24dc34f92f984d5b28b0c
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-12-15 23:49:43 +0530

    Time: 0 ms (100%), Space: 25.8 MB (99.58%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        if (height.size() == 0) return 0;
-
-        int maxi = 0;
-        int idx = 0;
-
-      
-        for (int i = 0; i < height.size(); i++) {
-            if (height[i] > maxi) {
-                maxi = height[i];
-                idx = i;
+        int totalWater=0;
+        int maxBuilding=0;
+        int indexMax=0;
+        for(int i=0; i<height.size(); i++){
+            if(height[i]>maxBuilding){
+                maxBuilding=height[i];
+                indexMax=i;
             }
         }
-
-        int totalWater = 0;
-
-      
-        int leftMax = height[0];
-        for (int i = 1; i < idx; i++) {
-            leftMax = max(leftMax, height[i]);
-            totalWater += leftMax - height[i];
+        int prev=height[0];
+        for(int i=0; i<indexMax; i++){
+            prev=max(prev,height[i]);
+            int water=prev-height[i];
+            totalWater+=water;
         }
-
-      
-        int rightMax = height[height.size() - 1];
-        for (int i = height.size() - 2; i > idx; i--) {
-            rightMax = max(rightMax, height[i]);
-            totalWater += rightMax - height[i];
+        int j=height.size()-1;
+        prev=height[j];
+        for(int i=j; i>indexMax; i--){
+            prev=max(prev,height[i]);
+            int water=prev-height[i];
+            totalWater+=water;
         }
-
         return totalWater;
     }
 };
