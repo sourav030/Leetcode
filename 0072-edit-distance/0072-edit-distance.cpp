b89880451b5d8b9f95f7456d41462430449a994d
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-12-16 13:18:50 +0530

    Time: 3 ms (91.93%), Space: 14.2 MB (23.4%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int solve(string word1,string word2, int n, int m){
-        if(n==0){
-            return m;
+    vector<vector<int>> dp;
+
+    int solve(string &word1, string &word2, int i, int j) {
+        if (i < 0) return j + 1;  // insert remaining
+        if (j < 0) return i + 1;  // delete remaining
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (word1[i] == word2[j]) {
+            return dp[i][j] = solve(word1, word2, i - 1, j - 1);
         }
-        if(m==0){
-            return n;
-        }
-        if(dp[n][m]!=-1){
-            return dp[n][m];
-        }
-        if(word1[n-1]==word2[m-1]){
-            return dp[n][m]=solve(word1,word2,n-1,m-1);
-        }
-        else{
-            return dp[n][m]=1+min({solve(word1,word2,n-1,m),solve(word1,word2,n-1,m-1),solve(word1,word2,n,m-1)});
-        }
+
+        return dp[i][j] = 1 + min({
+            solve(word1, word2, i - 1, j),     // delete
+            solve(word1, word2, i, j - 1),     // insert
+            solve(word1, word2, i - 1, j - 1)  // replace
+        });
     }
+
     int minDistance(string word1, string word2) {
-        int n=word1.length();
-        int m=word2.length();
-        dp.assign(n+1,vector<int>(m+1,-1));
-        return solve(word1,word2,n,m);
+        int n1 = word1.length();
+        int n2 = word2.length();
+        dp.assign(n1, vector<int>(n2, -1));
+        return solve(word1, word2, n1 - 1, n2 - 1);
     }
 };
