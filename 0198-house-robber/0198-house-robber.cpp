c4bf1246dcffbe16b7b6d1de76f0a8e5dab0377d
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-12-17 22:56:02 +0530

    Time: 0 ms (100%), Space: 10.7 MB (21.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int solve(vector<int>&nums, int idx,vector<int>&dp){
+    vector<int>dp;
+    int solve(vector<int>&nums, int idx){
         if(idx>=nums.size()){
             return 0;
         }
         if(dp[idx]!=-1){
             return dp[idx];
         }
-        return dp[idx]= max(nums[idx]+solve(nums,idx+2,dp),solve(nums,idx+1,dp));
+        int choriKiya=nums[idx]+solve(nums,idx+2);
+        int nhiChoriKiya=solve(nums,idx+1);
+        return dp[idx]=max(choriKiya,nhiChoriKiya);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        return solve(nums,0,dp);
+        dp.resize(n+1,-1);
+        return solve(nums,0);
+
     }
 };
