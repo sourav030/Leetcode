76a1768746e49bafec35453dcd8f1df425b3f01b
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2025-12-22 13:51:40 +0530

    Time: 2 ms (19.79%), Space: 9.6 MB (27.29%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    int row, col;
-    vector<vector<int>> dp;
+    vector<vector<int>>grid;
+    vector<vector<int>>dp;
+    vector<int>row={1,0};
+    vector<int>col={0,1};
+    int solve(int r, int c, int m, int n){
+        if(r==m-1 and c==n-1){
+            return 1;
+        }
+        if(dp[r][c]!=-1){
+            return dp[r][c];
+        }
+        int count=0;
+        grid[r][c]=1;
+        for(int i=0; i<2; i++){
+            int rows=r+row[i];
+            int cols=c+col[i];
+            if(rows>=0 and rows<m and cols>=0 and cols<n and !grid[rows][cols]){
+                count+=solve(rows,cols,m,n);
+            }
+        }
+        grid[r][c]=0;
+        return dp[r][c]= count;
 
-    int solve(int r, int c) {
-        if (r == row - 1 && c == col - 1) return 1; // reached destination
-        if (r >= row || c >= col) return 0;         // out of bounds
-
-        if (dp[r][c] != -1) return dp[r][c];        // already computed
-
-        int right = solve(r, c + 1);
-        int down = solve(r + 1, c);
-
-        return dp[r][c] = right + down;
     }
-
     int uniquePaths(int m, int n) {
-        row = m;
-        col = n;
-        dp.assign(m, vector<int>(n, -1)); 
-        return solve(0, 0);
+        grid.assign(m,vector<int>(n,0));
+        dp.assign(m+1,vector<int>(n+1,-1));
+        return solve(0,0,m,n);
     }
 };
