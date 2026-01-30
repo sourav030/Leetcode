3d18febccdcba22c8e4662dcdea13e8876f66b54
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2026-01-30 21:02:06 +0530

    Time: 0 ms (100%), Space: 10.2 MB (69.15%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,13 +3,12 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         for(int i=0; i<matrix.size(); i++){
             for(int j=0; j<i; j++){
-                swap(matrix[i][j],matrix[j][i]);
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-
         for(int i=0; i<matrix.size(); i++){
-            reverse(matrix[i].begin(),matrix[i].end());
+            reverse(matrix[i].begin(), matrix[i].end());
         }
-        
+        return ;
     }
 };
