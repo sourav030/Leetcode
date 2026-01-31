a8e9f30fcb4b5cdb82feeceb526dee95575f67c4
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2026-01-31 15:51:36 +0530

    Time: 0 ms (100%), Space: 28.2 MB (27.35%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+       
         int count=1;
-        if(nums.size()==1){
-            return nums[0];
-        }
+        int curr=nums[0];
         for(int i=1; i<nums.size(); i++){
-            if(nums[i]==nums[i-1]){
+            if(curr==nums[i]){
                 count++;
             }
             else{
+                count--;
+            }
+            if(count==0){
+                curr=nums[i];
                 count=1;
             }
-            if(count>nums.size()/2){
-                return nums[i];
-            }
         }
-        return -1;
+        return curr;
     }
 };
