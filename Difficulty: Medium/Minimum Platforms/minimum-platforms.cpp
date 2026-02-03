d8530b9c99b35cbdf1c464b7fdcd33a60756b3d1
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2026-02-03 14:10:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -1,31 +1,28 @@
 class Solution {
   public:
-    // Function to find the minimum number of platforms required at the
-    // railway station such that no train waits.
-   int findPlatform(vector<int>& arr, vector<int>& dep) {
-    sort(arr.begin(), arr.end());
-    sort(dep.begin(), dep.end());
+    int minPlatform(vector<int>& arr, vector<int>& dep) {
 
-    // Min-heap to store current departure times on platforms
-    priority_queue<int, vector<int>, greater<int>> pq;
+        sort(arr.begin(), arr.end());
+        sort(dep.begin(), dep.end());
 
-    pq.push(dep[0]);
-    int maxPlatform = 1;
+        priority_queue<int, vector<int>, greater<int>> pq;
 
-    for (int i = 1; i < arr.size(); i++) {
-        // Remove all trains that have already departed
-        while (!pq.empty() && arr[i] > pq.top()) {
-            pq.pop();
+        pq.push(dep[0]);
+        int ans = 1;
+
+        for(int i = 1; i < arr.size(); i++) {
+
+           
+            if(!pq.empty() && pq.top() < arr[i]) {
+                pq.pop();
+            }
+
+          
+            pq.push(dep[i]);
+
+            ans = max(ans, (int)pq.size());
         }
 
-        // Assign current train to a platform (new or reused)
-        pq.push(dep[i]);
-
-        // Max platforms needed so far
-        maxPlatform = max(maxPlatform, (int)pq.size());
+        return ans;
     }
-
-    return maxPlatform;
-}
-
 };
