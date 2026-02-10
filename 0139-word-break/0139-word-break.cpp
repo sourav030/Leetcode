7616d86a69edd428209031c5db6515f84aae731c
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2026-02-10 21:25:10 +0530

    Time: 2 ms (71.92%), Space: 12.1 MB (63.52%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    unordered_set<string> dict; // store wordDict
-    vector<int> memo; // -1 = not computed, 0 = false, 1 = true
-    
-    bool solve(int idx, const string &s) {
-        if (idx == s.length()) return true;
-        if (memo[idx] != -1) return memo[idx];
-        
-        for (int end = idx; end < s.length(); end++) {
-            string word = s.substr(idx, end - idx + 1);
-            if (dict.count(word) && solve(end + 1, s)) {
-                return memo[idx] = true;
+    unordered_map<string, int> mp;
+    unordered_map<int, bool> dp;   
+    int n;
+
+    bool solve(string &str, int idx) {
+
+        if (idx >= n)
+            return true;
+
+        if (dp.count(idx))
+            return dp[idx];
+
+        string word = "";
+
+        for (int i = idx; i < n; i++) {      
+            word += str[i];
+
+            if (mp.find(word) != mp.end() &&
+                solve(str, i + 1)) {
+
+                return dp[idx] = true;
             }
         }
-        
-        return memo[idx] = false;
+
+        return dp[idx] = false;
     }
-    
+
     bool wordBreak(string s, vector<string>& wordDict) {
-        dict.clear();
-        for (auto &w : wordDict) dict.insert(w);
-        memo.assign(s.length(), -1);
-        return solve(0, s);
+
+        for (auto &w : wordDict)
+            mp[w]++;
+
+        n = s.length();
+
+        return solve(s, 0);
     }
 };
