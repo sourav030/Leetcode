76cc84b3b6ab318706757b014cd53326ff06a3c6
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2026-02-11 17:06:07 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.1%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     double solve(double x, long long n) {
-        if (n == 0) return 1;
+        if(n == 0) return 1;
+        if(n == 1) return x;
 
-        double half = solve(x, n / 2);
+        double half = solve(x, n/2);
 
-        if (n % 2 == 0)
+        if(n % 2 == 0)
             return half * half;
         else
-            return half * half * x;
+            return x * half * half;
     }
 
     double myPow(double x, int n) {
-        long long N = n;  
+        long long N = n;        
 
-        if (N < 0) {
-            x = 1 / x;
+        if(N < 0) {
+            x = 1/x;
             N = -N;
         }
 
