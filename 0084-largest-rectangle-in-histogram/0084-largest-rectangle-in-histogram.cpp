31166f5885449887ba3b0fdcdd68b03deff8199a
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2026-02-17 14:31:36 +0530

    Time: 31 ms (28.54%), Space: 85.8 MB (33.49%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> left(n), right(n);
-        stack<int> st;
+        int n=heights.size();
+        vector<int>nextSmaller(n,n);
+        vector<int>prevSmaller(n,-1);
+        stack<int>st;
+        for(int i=n-1; i>=0; i--){
 
-        // previous smaller
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && heights[st.top()] >= heights[i]) {
+            while(!st.empty() and heights[i]<=heights[st.top()]){
                 st.pop();
             }
-
-            left[i] = st.empty() ? -1 : st.top();
+            if(!st.empty() and heights[i]>heights[st.top()]){
+                nextSmaller[i]=st.top();
+            }
             st.push(i);
         }
-
-        while (!st.empty()) st.pop();
-
-        // next smaller
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && heights[st.top()] >= heights[i]) {
+        while(!st.empty()){
+            st.pop();
+        }
+        for(int i=0; i<n; i++){
+             while(!st.empty() and heights[i]<=heights[st.top()]){
                 st.pop();
             }
-
-            right[i] = st.empty() ? n : st.top();
+            if(!st.empty() and heights[i]>heights[st.top()]){
+                prevSmaller[i]=st.top();
+            }
             st.push(i);
         }
-
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = max(ans, heights[i] * (right[i] - left[i] - 1));
+        int ans=INT_MIN;
+        for(int i=0; i<heights.size(); i++){
+            int h=heights[i];
+            int w=nextSmaller[i]-prevSmaller[i]-1;
+            ans=max(ans,h*w);
         }
         return ans;
     }
