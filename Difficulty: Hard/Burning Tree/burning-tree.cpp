a6a66f1e1fccbe44ff9f034e14e5e2172280fbbb
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2026-02-21 15:22:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Burning Tree/burning-tree.cpp
+++ Difficulty: Hard/Burning Tree/burning-tree.cpp
@@ -11,6 +11,7 @@ class Node {
     }
 };
 */
+
 class Solution {
   public:
     unordered_map<int,vector<int>>mp;
@@ -21,38 +22,37 @@ class Solution {
             Node* node=q.front();
             q.pop();
             if(node->left){
+                q.push(node->left);
                 mp[node->data].push_back(node->left->data);
                 mp[node->left->data].push_back(node->data);
-                q.push(node->left);
             }
             if(node->right){
-                 mp[node->data].push_back(node->right->data);
-                mp[node->right->data].push_back(node->data);
                 q.push(node->right);
+                mp[node->data].push_back(node->right->data);
+                mp[node->right->data].push_back(node->data);
             }
         }
     }
-    
     int minTime(Node* root, int target) {
         // code here
         buildGraph(root);
-        vector<bool>visited(pow(10,5),0);
-        queue<pair<int,int>>q;
-        q.push({target,0});
+        unordered_set<int>st;
         int time=-1;
-        visited[target]=1;
+        queue<int>q;
+        q.push(target);
         while(!q.empty()){
-            auto node=q.front();
-            time=max(time,node.second);
-            q.pop();
-            
-            for(auto ele:mp[node.first]){
-                if(visited[ele]==0){
-                    visited[ele]=1;
-                    q.push({ele,node.second+1});
+            int n=q.size();
+            for(int i=0; i<n; i++){
+                int node=q.front();
+                q.pop();
+                st.insert(node);
+                for(auto ele:mp[node]){
+                    if(st.find(ele)==st.end()){
+                        q.push(ele);
+                    }
                 }
             }
-            
+            time++;
         }
         return time;
         
