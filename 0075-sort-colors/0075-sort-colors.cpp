19a768ed4924002653530f6a7ce5f1659cdf512b
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2026-03-08 17:18:34 +0530

    Time: 0 ms (100%), Space: 11.6 MB (48.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,20 +4,18 @@ public:
         int start=0;
         int mid=0;
         int end=nums.size()-1;
-        
         while(mid<=end){
             if(nums[mid]==0){
-                swap(nums[mid],nums[start]);
+                swap(nums[start],nums[mid]);
                 start++;
                 mid++;
-                
             }
             else if(nums[mid]==1){
                 mid++;
             }
             else{
                 swap(nums[mid],nums[end]);
-                end--; 
+                end--;
             }
         }
     }
