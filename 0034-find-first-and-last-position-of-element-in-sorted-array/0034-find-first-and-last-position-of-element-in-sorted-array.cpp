c789a221e9c1ecc1873acc430e318c4ef4492b6a
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2026-03-16 10:27:31 +0530

    Time: 0 ms (100%), Space: 18.2 MB (19.21%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,48 +1,45 @@
 class Solution {
 public:
-    int firstPos(vector<int>& nums, int target) {
-        int start = 0, end = nums.size() - 1;
-        int ans = -1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                end = mid - 1;   
+    int LowerBound(vector<int>nums, int target){
+        int start=0;
+        int end=nums.size()-1;
+        int ans=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                end=mid-1;
             }
-            else if (nums[mid] < target) {
-                start = mid + 1;
+            else if(nums[mid]<target){
+                start=mid+1;
             }
-            else {
-                end = mid - 1;
-            }
-        }
-        return ans;
-    }
-
-    int lastPos(vector<int>& nums, int target) {
-        int start = 0, end = nums.size() - 1;
-        int ans = -1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                start = mid + 1;  
-            }
-            else if (nums[mid] < target) {
-                start = mid + 1;
-            }
-            else {
-                end = mid - 1;
+            else{
+                end=mid-1;
             }
         }
         return ans;
     }
-
+     int UpperBound(vector<int>nums, int target){
+        int start=0;
+        int end=nums.size()-1;
+        int ans=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            cout<<mid<<" ";
+            if(nums[mid]==target){
+                ans=mid;
+                start=mid+1;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        return { firstPos(nums, target), lastPos(nums, target) };
+        return {LowerBound(nums,target),UpperBound(nums,target) };
     }
 };
