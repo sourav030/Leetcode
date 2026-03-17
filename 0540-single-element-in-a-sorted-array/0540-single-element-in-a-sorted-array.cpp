5e15bf65287e96039e5b67f4bd2ab86f5e79972c
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2026-03-17 08:32:08 +0530

    Time: 0 ms (100%), Space: 26.3 MB (34.32%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int start=0;
-        int end=nums.size()-1;
-        if(nums.size()==1) return nums[0];
-        if(nums[start]!=nums[start+1]) return nums[start];
-        if(nums[end]!=nums[end-1]) return nums[end];
-        
-        while(start<=end){
-            int mid=start+(end-start)/2;
-
-            if(nums[mid]!=nums[mid+1] and nums[mid]!=nums[mid-1]){
-                return nums[mid];
-            }
-
-           if((mid%2==1 and nums[mid]==nums[mid-1]) or (mid%2==0 and nums[mid]==nums[mid+1])){
-             start = mid + 1;
-            }
-        else{
-             end = mid - 1;
+        int ans=0;
+        for(auto num:nums){
+            ans^=num;
         }
-        }
-        return -1;
+        return ans;
     }
 };
