6e8e4634746b43562bab543213b8ea2bff6dec2e
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2026-03-17 08:37:14 +0530

    Time: 0 ms (100%), Space: 12.6 MB (70.07%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return 0;
-
-        int start = 0;
-        int end = n - 1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            // Handle boundaries safely
-            long left = (mid - 1 >= 0) ? nums[mid - 1] : LONG_MIN;
-            long right = (mid + 1 < n) ? nums[mid + 1] : LONG_MIN;
-
-            if (nums[mid] > left && nums[mid] > right) {
-                return mid; // Found peak
+        if(nums.size()==1){
+            return 0;
+        }
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+        int n=nums.size()-1;
+        if(nums[n]>nums[n-1]){
+            return n;
+        }
+        int start=1;
+        int end=nums.size()-2;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]>nums[mid+1] and nums[mid]>nums[mid-1]){
+                return mid;
             }
-
-            if (nums[mid] < right) {
-                start = mid + 1; // Peak must be on right side
-            } else {
-                end = mid - 1; // Peak must be on left side
+            else if(nums[mid]<nums[mid+1]){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
             }
         }
-        return -1; // Should never reach here
+        return -1;
     }
 };
