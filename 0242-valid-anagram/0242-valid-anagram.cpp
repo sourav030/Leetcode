fb849f8d69dad500da959f898fd19349bb4c52e5
Sourav Kumar Tiwari <128700861+sourav030@users.noreply.github.com>
2026-03-24 10:35:07 +0530

    Time: 100 ms (7.35%), Space: 9.8 MB (47.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int>arr(26,0);
-        for(int i=0; i<s.length(); i++){
-            arr[s[i]-'a']++;
-        }
-        for(int i=0; i<t.length(); i++){
-            arr[t[i]-'a']--;
-        }
-        for(int i=0; i<26; i++){
+       if(s.length()!=t.length()){
+        return false;
+       }
+       vector<int>arr(26,0);
+       for(int i=0; i<s.length(); i++){
+         arr[s[i]-'a']++;
+          cout<<arr[s[i]-'a'];
+       }
+
+       for(int i=0; i<t.length(); i++){
+         arr[t[i]-'a']--;
+        
+       }
+
+       for(int i=0; i<26; i++){
             if(arr[i]!=0){
-                return false;
+             return false;
             }
-        }
-        return true;
+            
+       }
+       return true;
+
     }
 };
